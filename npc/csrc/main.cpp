b9a537f0374d084ab744cfb6e6392a2cb68c6923
tracer-ysyx <tracer@ysyx.org>
2025-10-09 17:19:18 +0800

    > sim RTL ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:19:18 up 17:24,  1 user,  load average: 0.47, 0.21, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -14,18 +14,26 @@ int main(int argc, char **argv)
   Vtop *top = new Vtop{contextp};
 
   // https://verilator.org/guide/latest/faq.html#how-do-i-generate-waveforms-traces-in-c
+  Verilated::traceEverOn(true);
   VerilatedVcdC *tfp = new VerilatedVcdC;
   top->trace(tfp, 99);
-  while (!contextp->gotFinish())
+  tfp->open("waveform.vcd");
+
+  uint64_t sim_tim = 50;
+
+  while ((contextp->time()<sim_tim)&&(!contextp->gotFinish()))
   {
+    contextp->timeInc(1);
     int a = rand() & 1;
     int b = rand() & 1;
     top->a = a;
     top->b = b;
     top->eval();
+    tfp->dump(contextp->time());
     printf("a=%d, b=%d, f=%d\n", a, b, top->f);
     assert(top->f == (a ^ b));
   }
+  tfp->close();
   delete top;
   delete contextp;
   return 0;
