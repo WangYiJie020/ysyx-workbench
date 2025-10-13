03b3177ca0fadfca0b5ad48859ea0fd7d8a11b8e
tracer-ysyx <tracer@ysyx.org>
2025-10-13 15:01:20 +0800

    > sim RTL ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:01:20 up 12:11,  1 user,  load average: 0.85, 0.91, 0.78

--- npc/vsrc/ps2test.v
+++ npc/vsrc/ps2test.v
@@ -23,7 +23,7 @@ module ps2test(
             else ready<=0;
         end else begin
             ready<=done;
-            ready_mantain<=32'h004F_FFFF;
+            ready_mantain<=32'h003F_FFFF;
         end
     end
 endmodule
