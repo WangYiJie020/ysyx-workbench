71acddaa115c1a840f1908d8fca733580ffd6d5e
tracer-ysyx <tracer@ysyx.org>
2025-10-13 12:31:30 +0800

    > sim RTL ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:31:30 up 10:47,  1 user,  load average: 0.64, 0.53, 0.37

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -41,12 +41,13 @@ module top(
     end
     assign notidle = ~idle;
     assign ledr[0]=idle;
+    assign ledr[1]=ps2_ready;
     bcd7seg seghigh(
         .bcd(ps2_ready?ps2_out[7:4]:0),
         .seg(seg1)
     );
     bcd7seg seglow(
-        .bcd(notidle?ps2_out[3:0]:0),
+        .bcd(ps2_ready?ps2_out[3:0]:0),
         .seg(seg0)
     );
 endmodule
