fa963e8c2926738930d76a29907ce97fec2090da
tracer-ysyx <tracer@ysyx.org>
2025-10-14 20:02:34 +0800

    > sim RTL ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:02:34 up  1:39,  1 user,  load average: 0.14, 0.04, 0.01

--- npc/vsrc/scpu.v
+++ npc/vsrc/scpu.v
@@ -4,31 +4,15 @@ module scpu(
     output [3:0] nxt_pc,
     output [7:0] out,
 
-    input [7:0] rin[4],
-    output reg [7:0] rout[4]
+    input [31:0] rinbus,
+    output [31:0] routbus
 );
     parameter ADD=2'b00,LI=2'b10,BNER0=2'b11;
     parameter OUT=2'b01;
 
-    wire [1:0] op = code[7:6];
-    wire [1:0] rd = code[5:4];
-    wire [1:0] rs1 = code[3:2];
-    wire [1:0] rs2 = code[1:0];
-    wire [7:0] imm = {4'b0, code[3:0]};
-    wire [3:0] addr = code[5:2];
-
-    always @(*) begin
-        rout[0] = rin[0];
-        rout[1] = rin[1];
-        rout[2] = rin[2];
-        rout[3] = rin[3];
-        case (op)
-            ADD: rout[rd] = rin[rs1] + rin[rs2];
-            LI: rout[rd] = imm;
-            default: ;
-        endcase
-    end
-    assign nxt_pc = (op==BNER0 && rin[rs2]!=rin[0]) ? addr : (pc + 1);
-
-    assign out = (op==OUT) ? rin[rd] : 8'b0;
+    wire [7:0] rin[0:3];
+    assign rin[0] = rinbus[7:0];
+    assign rin[1] = rinbus[15:8];
+    assign rin[2] = rinbus[23:16];
+    assign rin[3] = rinbus[31:24];
 endmodule
