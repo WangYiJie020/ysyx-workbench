4e88cd0d8dff9a3daa5ceb00793c6dc71677cd78
tracer-ysyx <tracer@ysyx.org>
2025-10-14 13:24:29 +0800

    > sim RTL ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:24:29 up 22:37,  1 user,  load average: 0.29, 0.08, 0.02

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -24,6 +24,49 @@ module top(
     output [7:0] seg6,
     output [7:0] seg7
 );
+wire [9:0] h_addr;
+wire [9:0] v_addr;
+wire [23:0] vga_data;
+
+vga_ctrl my_vga_ctrl(
+    .pclk(clk),
+    .reset(rst),
+    .vga_data(vga_data),
+    .h_addr(h_addr),
+    .v_addr(v_addr),
+    .hsync(VGA_HSYNC),
+    .vsync(VGA_VSYNC),
+    .valid(VGA_BLANK_N),
+    .vga_r(VGA_R),
+    .vga_g(VGA_G),
+    .vga_b(VGA_B)
+);
+
+vmem my_vmem(
+    .h_addr(h_addr),
+    .v_addr(v_addr[8:0]),
+    .vga_data(vga_data)
+);
+
+endmodule
+
+module vmem(
+    input [9:0] h_addr,
+    input [8:0] v_addr,
+    output [23:0] vga_data
+);
+
+reg [23:0] vga_mem [524287:0];
+
+initial begin
+    $readmemh("resource/picture.hex", vga_mem);
+end
+
+assign vga_data = vga_mem[{h_addr, v_addr}];
+
+endmodule
+/*Exp7
+
 initial begin
     {seg0,seg1,seg2,seg3,seg4,seg5,seg6,seg7}=64'hff_ff_ff_ff_ff_ff_ff_ff;
 end
@@ -105,4 +148,4 @@ end
         end
     end
     assign ledr[3]=idle;
-endmodule
+*/
