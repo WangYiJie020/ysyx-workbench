e70605654fdf82b806dc97febbf549525463a2ae
tracer-ysyx <tracer@ysyx.org>
2025-10-15 23:19:56 +0800

    > compile NEMU ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:19:56 up 15:57,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,44 +30,8 @@ int main(int argc, char *argv[]) {
 #endif
 
   /* Start engine. */
-  //engine_start();
+  engine_start();
 
-  // read ./tools/gen-expr/input
-
-  FILE *fp = fopen("./tools/gen-expr/input2", "r");
-  if (fp == NULL) {
-	perror("Failed to open input file");
-	return 1;
-  }
-  char buffer[65536];
-
-  // line 1 expected result
-  // line 2 expression
-  // line 3 expected result
-  // line 4 expression
-  // ...
-  while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-	char *endptr;
-	long expected = strtol(buffer, &endptr, 10);
-	if (endptr == buffer || (*endptr != '\n' && *endptr != '\0')) {
-	  fprintf(stderr, "Invalid number: %s", buffer);
-	  assert(0);
-	}
-	if (fgets(buffer, sizeof(buffer), fp) == NULL) {
-	  fprintf(stderr, "Missing expression after expected result: %ld\n", expected);
-	  assert(0);
-	}
-	bool success = true;
-	long result = expr(buffer, &success);
-	if (!success) {
-		//fprintf(stderr, "Failed to evaluate expression: %s", buffer);
-		continue;
-	}
-	if (result != expected) {
-		fprintf(stderr, "Expression: %sExpected: %ld, Got: %ld\n", buffer, expected, result);
-		continue;
-	}
-  }
 
   return is_exit_status_bad();
 }
