dd45b3cc278cbb838805f855a5a357cca94a2840
tracer-ysyx <tracer@ysyx.org>
2025-10-16 00:59:32 +0800

    > compile NEMU ysyx_22040000 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:59:32 up 17:32,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -268,7 +268,7 @@ word_t eval(int p, int q, bool *success) {
 		return val;
 	}
 	else if(tokens[p].type == TK_REG) {
-		 printf("Evaluating register %s\n", tokens[p].str);
+		 //printf("Evaluating register %s\n", tokens[p].str);
 		 word_t v = isa_reg_str2val(tokens[p].str + 1, success);
 		 if(!(*success)) {
 			 printf("Error: unknown register %s\n", tokens[p].str);
