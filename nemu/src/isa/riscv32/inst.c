2ff320d29a5b4659ce9199498956c95965a7888d
tracer-ysyx <tracer@ysyx.org>
2025-10-31 23:53:03 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:53:03 up 1 day,  6:22,  0 user,  load average: 0.11, 0.03, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -89,7 +89,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
 word_t d_sext(word_t v,int len){
   uint32_t spos=len-1; // for len==0 BITS use shift make v[spos]=0
   word_t  mask=(1U << len) - 1U;
-  printf("sign of %08X v[%d]=%d | mask %08X\n",v,len,(int)BITS(v,spos,spos),mask);
+  printf("sign of %08X v[%d]=%d | mask %08X\n",v,len,(int)BITS(v,spos,spos),1U<<len);
 	if(BITS(v,spos,spos))v|=~mask;
 	return v;
 }
