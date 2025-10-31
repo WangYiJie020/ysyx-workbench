0609841ef967cb18b84e4859ff67139419bcc9b2
tracer-ysyx <tracer@ysyx.org>
2025-11-01 00:55:17 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:55:17 up 1 day,  7:24,  0 user,  load average: 0.10, 0.07, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -22,6 +22,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
     printf("call strcmp %s, %s\n",s1,s2);
+    return 0;
     int tmp;
     while(*s1&&*s2){
          tmp = (int)*s1-(int)*s2;
