8e8f300419dca61f62253c5728058b6225daa89b
tracer-ysyx <tracer@ysyx.org>
2025-11-01 01:54:34 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:54:34 up 1 day,  8:23,  0 user,  load average: 0.05, 0.03, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,44 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+#define _putch(ch) do{\
+    *out=ch;out++;\
+    cnt++;\
+}while(0)
+
+    int cnt=0;
+    va_list ap;
+    va_start(ap,fmt);
+    while(*fmt){
+        if(*fmt=='%'){
+            fmt++;
+            switch(*fmt){
+                case 's':{
+                     const char* str=va_arg(ap,const char*);
+                     while(*str){
+                         _putch(*str);
+                         str++;
+                     }
+                     break;
+                         }
+                case 'd':{
+                     int d=va_arg(ap,int);
+                     if(d<0){
+                         _putch('-');
+                         d=-d;
+                     }
+                     while(d){
+                         _putch('0'+d%10);
+                         d/=10;
+                     }
+                     break;
+                         }
+            }
+        }
+        else _putch(*fmt);
+    }
+    va_end(ap);
+    return cnt;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
