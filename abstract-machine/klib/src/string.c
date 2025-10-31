c234ef69a3266d2bd37c2664b4b8ef85274003d2
tracer-ysyx <tracer@ysyx.org>
2025-11-01 00:51:52 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:51:52 up 1 day,  7:20,  0 user,  load average: 0.03, 0.03, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -21,8 +21,14 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-    return 0;
-  //panic("Not implemented");
+    int tmp;
+    while(*s1&&*s2){
+         tmp = (int)*s1-(int)*s2;
+         if(tmp)return tmp;
+         s1++;
+         s2++;
+    }
+    return (int)*s1-(int)*s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
