74b22d51b2f710359d0e60567d3082b0621827ae
tracer-ysyx <tracer@ysyx.org>
2025-11-01 01:32:53 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:32:53 up 1 day,  8:01,  0 user,  load average: 0.02, 0.02, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,6 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-    return 0;
   panic("Not implemented");
 }
 
@@ -21,7 +20,6 @@ char *strcpy(char *dst, const char *src) {
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-    return 0;
   panic("Not implemented");
 }
 
@@ -48,7 +46,6 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-    return 0;
   panic("Not implemented");
 }
 
@@ -63,12 +60,10 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-    return 0;
   panic("Not implemented");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-    return 0;
   panic("Not implemented");
 }
 
