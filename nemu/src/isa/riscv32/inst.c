2fcbcf6c7068e967954423394afdfd89f5babce0
tracer-ysyx <tracer@ysyx.org>
2025-10-31 11:10:24 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:10:24 up 22:13,  0 user,  load average: 0.04, 0.08, 0.03

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -82,7 +82,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     //  printf("  |decode:| rd %d r1 %d r2 %d imm %X(%d)\n",*rd,rs1,rs2,*imm,*imm);
 }
 
-word_t dynamic_sext(word_t v,int len){
+// sign ext v with dynamic len
+word_t d_sext(word_t v,int len){
 	v &= (1U << len) - 1;
 	if(v >> (len - 1))v|=~((1U << len) - 1);
 	return v;
@@ -128,7 +129,7 @@ static int decode_exec(Decode *s) {
   INSTPAT_R("0000000 ????? ????? 011 ????? 01100 11", sltu   , R(rd) = (src1<src2)?1:0); 
 
   word_t shamt=BITS(s->isa.inst,24,20);
-  INSTPAT_I("0100000 ????? ????? 101 ????? 00100 11", srai   , R(rd) = dynamic_sext(src1>>shamt,32-shamt)); 
+  INSTPAT_I("0100000 ????? ????? 101 ????? 00100 11", srai   , R(rd) = d_sext(src1>>shamt,32-shamt)); 
   INSTPAT_I("0000000 ????? ????? 101 ????? 00100 11", srli   , R(rd) = src1 >> shamt);
   INSTPAT_I("0000000 ????? ????? 001 ????? 00100 11", slli   , R(rd) = src1 << shamt);
 
