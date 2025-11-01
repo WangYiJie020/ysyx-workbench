4a8793dccd4126e92c34c5059cf3cc6493462e3c
tracer-ysyx <tracer@ysyx.org>
2025-11-01 12:29:38 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:29:38 up 1 day, 12:08,  0 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,7 +52,8 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-	printf("mem r %08X %db\n",addr,len);
+  IFDEF(CONFIG_MTRACE,printf("mem r %08X %db\n",addr,len));
+
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -60,7 +61,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-	printf("mem w %08X %db %08X\n",addr,len,data);
+	IFDEF(CONFIG_MTRACE,printf("mem w %08X %db %08X\n",addr,len,data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
