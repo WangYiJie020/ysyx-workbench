a276319e0a9fcf39cd72d5f57567187445bcef07
tracer-ysyx <tracer@ysyx.org>
2025-11-02 20:39:49 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:39:49 up  5:50,  1 user,  load average: 0.30, 0.10, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -18,9 +19,27 @@ void nvboard_bind_all_pins(TOP_NAME* top);
 void nvboard_update();
 void nvboard_init(int vga_clk_cycle);
 
+typedef uint32_t word_t;
+
+word_t mem[256]={
+	0x01400513,
+	0x010000e7,
+	0x00c000e7,
+	0x00c00067,
+	0x00a50513,
+	0x00a50513
+};
+
+uint32_t pmem_read(uint32_t addr){
+	return mem[addr];
+}
+
 static void single_cycle() {
     dut.clk=0;dut.eval();
 //	nvboard_update();
+
+	dut.inst=pmem_read(dut.pc);
+
     dut.clk=1;dut.eval();
 	nvboard_update();
 }
