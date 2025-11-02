81f20a383eaec8b705e29badcef238aeb14ab38f
tracer-ysyx <tracer@ysyx.org>
2025-11-02 23:39:47 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:39:47 up  8:47,  1 user,  load average: 0.19, 0.07, 0.02

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -5,6 +5,32 @@ import "DPI-C" function void raise_break ();
 
 localparam TypeN=0,TypeR=1,TypeI=2,TypeS=3,TypeU=4,TypeJ=5,TypeB=6;
 
+module cpu(
+    input [WORD_BITWIDTH-1:0] pc,
+    output [WORD_BITWIDTH-1:0] nxt_pc
+);
+endmodule
+
+module itype_decoder(
+    input [6:0] opcode,
+    output [3:0] itype
+);
+// opcode[1:0] should always be 11 for 32bit
+
+wire [4:0] opcu=opcode[6:2];
+
+wire isI=(opcu==5'b00100)||(opcu==5'b11001)||(opcu==5'b00000);
+wire isR=(opcu==5'b01100);
+wire isS=(opcu==5'b01000);
+
+assign itype=
+    isI?TypeI:
+    isR?TypeR:
+    isS?TypeS:
+    TypeN;
+
+endmodule
+
 module decode_operand(
     input [WORD_BITWIDTH-1:0] inst,
     output [3:0] itype,
@@ -17,8 +43,8 @@ module decode_operand(
     assign rd=inst[11:7];
     assign rs1=inst[19:15];
     assign rs2=inst[24:20];
-    assign itype=(opcode==7'b0010011)?TypeI:
-        (opcode==7'b1100111)?TypeJ:TypeN;
+
+    itype_decoder _idc(.opcode(opcode),.itype(itype));
 
     wire [31:0] immI={{21{inst[31]}},inst[30:20]};
 
@@ -35,9 +61,4 @@ module decode_operand(
 endmodule
 
 
-module cpu(
-    input [WORD_BITWIDTH-1:0] pc,
-    output [WORD_BITWIDTH-1:0] nxt_pc
-);
 
-endmodule
