b38fa53a720bf9dade4d30b2e539cd7a3d91221a
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2025-11-03 13:23:27 +0800

    (npc) impl add, lui make RegisterFile dump info when ebreak

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -5,7 +5,6 @@ parameter int REG_ADDRWIDTH=5;
 
 parameter int INST_EBREAK=32'h00100073;
 
-import "DPI-C" function void raise_break();
 
 localparam TypeN=0,TypeR=1,TypeI=2,TypeS=3,TypeU=4,TypeJ=5,TypeB=6;
 
@@ -18,9 +17,13 @@ endmodule
 module itype_decoder(
     input [6:0] opcode,
     output [3:0] itype,
+
     output is_arithmetic,
     output is_load,
-    output is_jalr
+    output is_jalr,
+
+    output is_lui,
+    output is_auipc
 );
 // opcode[1:0] should always be 11 for 32bit
 
@@ -29,15 +32,19 @@ wire [4:0] opcu=opcode[6:2];
 assign is_arithmetic=(opcu==5'b00100);
 assign is_jalr=(opcu==5'b11001);
 assign is_load=(opcu==5'b00000);
+assign is_lui=(opcu==5'b01101);
+assign is_auipc=(opcu==5'b00101);
 
 wire isI=is_arithmetic|is_jalr|is_load;
 wire isR=(opcu==5'b01100);
 wire isS=(opcu==5'b01000);
+wire isU=is_lui|is_auipc;
 
 assign itype=
     isI?TypeI:
     isR?TypeR:
     isS?TypeS:
+    isU?TypeU:
     TypeN;
 
 endmodule
@@ -50,7 +57,10 @@ module decode_operand(
 
     output is_arithmetic,
     output is_load,
-    output is_jalr
+    output is_jalr,
+
+    output is_lui,
+    output is_auipc
 );
 
     wire [6:0] opcode=inst[6:0];
@@ -62,7 +72,9 @@ module decode_operand(
     itype_decoder _idc(.opcode(opcode),.itype(itype),
         .is_jalr(is_jalr),
         .is_arithmetic(is_arithmetic),
-        .is_load(is_load)
+        .is_load(is_load),
+        .is_lui(is_lui),
+        .is_auipc(is_auipc)
     );
 
     wire [31:0] immI={{21{inst[31]}},inst[30:20]};
@@ -72,7 +84,6 @@ module decode_operand(
     wire [31:0] immJ={immI[31:20],inst[19:12],inst[20],inst[30:21],1'b0};
 
     always@(*)begin
-        if(inst==INST_EBREAK)raise_break();
 
         case(itype)
             TypeI:imm=immI;
@@ -80,7 +91,10 @@ module decode_operand(
             TypeS:imm=immS;
             TypeB:imm=immB;
             TypeU:imm=immU;
-            default:imm=32'hBAADF00D;
+            default:begin
+                $display("UNKNOWN Inst Type");
+                imm=32'hBAADF00D;
+            end
         endcase
     end
 
@@ -89,6 +103,7 @@ endmodule
 parameter int BADCALL_RESVALUE=32'hBAADCA11;
 
 module alu(
+    input is_imm, // in imm mode unuse func7t
     input [2:0] func3t,
     input [6:0] func7t,
     input `WORD_RANGE src1,src2,
@@ -98,11 +113,21 @@ module alu(
 always@(*)begin
     case(func3t)
         3'b000:begin
-            if(func7t==7'b0)res=src1+src2;
-            else res=BADCALL_RESVALUE;
+            if(is_imm)res=src1+src2;
+            else begin
+                if(func7t==7'b0)res=src1+src2;
+                else begin
+                    res=BADCALL_RESVALUE;
+                    $display("(alu) UNKNOWN func7t %d",func7t);
+                end
+            end
         end
-        default:res=BADCALL_RESVALUE;
+        default:begin
+            res=BADCALL_RESVALUE;
+            $display("(alu) UNKNOWN func3t %d",func3t);
+        end
     endcase
+    $display("(alu) src1:%08X src2:%08X -> res=%08X",src1,src2,res);
 end
 
 endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,3 +1,4 @@
+import "DPI-C" function void raise_break();
 import "DPI-C" function int pmem_read(input int raddr);
 import "DPI-C" function void pmem_write(
   input int waddr, input int wdata, input byte wmask);
@@ -39,9 +40,8 @@ module top(
     reg [WORD_BITWIDTH-1:0] wdata,nxt_pc;
     wire [REG_ADDRWIDTH-1:0] rd,rs1,rs2;
 
-    wire is_arithmetic;
-    wire is_load;
-    wire is_jalr;
+    wire is_arithmetic, is_load, is_jalr;
+    wire is_lui,is_auipc;
 
     RegisterFile #(
         .ADDR_WIDTH(REG_ADDRWIDTH),
@@ -52,7 +52,8 @@ module top(
         .wdata(wdata),
         .raddr1(rs1),.raddr2(rs2),
         .rdata1(src1),.rdata2(src2),
-        .wen(wen)
+        .wen(wen),
+        .dump_info(inst==INST_EBREAK)
     );
 
     decode_operand dec_opr(
@@ -62,9 +63,12 @@ module top(
         .rd(rd),
         .rs1(rs1),
         .rs2(rs2),
+
         .is_jalr(is_jalr),
         .is_arithmetic(is_arithmetic),
-        .is_load(is_load)
+        .is_load(is_load),
+        .is_lui(is_lui),
+        .is_auipc(is_auipc)
     );
 
     wire [6:0] opcode=inst[6:0];
@@ -74,32 +78,57 @@ module top(
     // use alu only when TypeR/TypeI
     assign alu_s1=src1;
     assign alu_s2=(itype==TypeR)?src2:imm;
+    wire is_imm;
+    assign is_imm=(itype==TypeI);
 
-    alu _alu(.func3t(func3t),.func7t(func7t),
-        .src1(alu_s1),.src2(alu_s1),.res(alu_res)
+    alu _alu(
+        .is_imm(is_imm),
+        .func3t(func3t),.func7t(func7t),
+        .src1(alu_s1),.src2(alu_s2),.res(alu_res)
     );
 
 
     assign nxt_pc=is_jalr?((src1+imm)&~1):(pc+4);
 
-    always@(*)begin
-        wen=0;wdata=0;
+    assign wen=(itype!=TypeS)&&(itype!=TypeN);
 
-        if(is_jalr)begin
-            wdata=pc+4;
-            wen=1;
-            $display("JALR %08X",nxt_pc);
-        end
-        else if(is_arithmetic)begin
-            wdata=alu_res;
-            wen=1;
-            $display("Write arithemtic result to r%d",rd);
-        end
+    always@(*)begin
+        wdata=32'hCDCDCDCD;
+        case(itype)
+            TypeI:begin
+                if(is_jalr)begin
+                    wdata=pc+4;
+                end else if(is_arithmetic)begin
+                    wdata=alu_res;
+                end else if(is_load)begin
+                end
+            end
+            TypeR:wdata=alu_res;
+            TypeU:begin
+                wdata=imm;
+            end
+            default:;
+
+        endcase
 
     end
 
     always@(posedge clk,posedge rst)begin
-        $display("rs1(r%d)=%d rs2(r%d)=%d imm=%d",rs1,src1,rs2,src2,imm);
+        $display("pc %08x: inst %08X",pc,inst);
+        if(inst==INST_EBREAK)begin
+            raise_break();
+        end
+        $display("rs1(r%d)=%08X(%d) rs2(r%d)=%08X(%d) imm=%08X(%d)",
+            rs1,src1,src1,
+            rs2,src2,src2,
+            imm,imm);
+        if(is_jalr)$display("JALR %08X",nxt_pc);
+        if(is_arithmetic)$display("Write arithemtic result");
+        if(is_load)$display("Load");
+        if(is_lui)$display("LUI");
+
+        if(wen)$display("update r%d <- %08X(%d)",rd,wdata,wdata);
+
         if(rst)begin
             pc<=0;
         end else begin
