8ce38fe27a4d57b66af2c6a90cd6400766314e55
tracer-ysyx <tracer@ysyx.org>
2025-11-03 20:31:17 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:31:17 up 17:20,  1 user,  load average: 0.21, 0.07, 0.07

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -39,8 +39,10 @@ module top(
 initial begin
     pc=INIT_PC;
 end
+    reg is_halted;
 
-    wire [WORD_BITWIDTH-1:0] inst=pmem_read(pc);
+    wire [WORD_BITWIDTH-1:0] inst=is_halted?0:pmem_read(pc);
+
 
     wire wen;
     wire [3:0] itype;
@@ -156,6 +158,7 @@ end
 
     always@(posedge clk,posedge rst)begin
         if(inst==INST_EBREAK)begin
+            is_halted<=1;
             raise_break();
         end else begin
 
