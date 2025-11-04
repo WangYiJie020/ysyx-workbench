ee45f9aabdf1d8a9a819c97d00ec31bf366c7bc4
tracer-ysyx <tracer@ysyx.org>
2025-11-04 16:14:59 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:14:59 up  6:02,  1 user,  load average: 0.24, 0.07, 0.02

--- abstract-machine/scripts/minirv-npc.mk
+++ abstract-machine/scripts/minirv-npc.mk
@@ -7,7 +7,7 @@ AS = minirv-gcc
 CXX = minirv-g++
 
 #COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
-COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite
+COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
 AM_SRCS += riscv/npc/libgcc/div.S \
