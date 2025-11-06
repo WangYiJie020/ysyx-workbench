9e3d50c122ae8ad9ece152c5f65a0d6b5e723760
tracer-ysyx <tracer@ysyx.org>
2025-11-06 09:27:50 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:27:50 up 1 day,  3:30,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <common.h>
+#include <stdio.h>
 
 extern uint64_t g_nr_guest_inst;
 
@@ -28,13 +29,13 @@ void init_log(const char *log_file) {
     log_fp = fp;
   }
   else{
-//	  Log(ANSI_FMT("WARN: no log_file set, write_log will not work", ANSI_FG_YELLOW));
+	  Log(ANSI_FMT("WARN: no log_file set, write_log will not work", ANSI_FG_YELLOW));
   }
   Log("Log is written to %s", log_file ? log_file : "stdout");
 }
 
 bool log_enable() {
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
-         (g_nr_guest_inst <= CONFIG_TRACE_END), false);
+         (g_nr_guest_inst <= CONFIG_TRACE_END) && (log_fp!=stdout), false);
 }
 #endif
