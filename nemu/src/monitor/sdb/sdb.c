1677899319df13a2c9fbe250043f3bcdecb15290
tracer-ysyx <tracer@ysyx.org>
2025-11-10 23:11:25 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:11:25 up 3 days, 17:26,  1 user,  load average: 1.00, 1.02, 1.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,6 +53,7 @@ static int cmd_c(char *args) {
 
 int is_exit_status_bad(); 
 static int cmd_q(char *args) {
+	puts("foo____ cmd_q");
   if(is_exit_status_bad())return -1;
   else set_nemu_state(NEMU_QUIT,0,0);
   return -1;
