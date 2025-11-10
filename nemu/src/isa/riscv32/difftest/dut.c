b762e449cb7b7cfcad9582b572981ac273162112
tracer-ysyx <tracer@ysyx.org>
2025-11-10 20:03:11 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:03:11 up 3 days, 15:22,  1 user,  load average: 1.04, 1.06, 1.01

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -26,7 +26,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	for(int i=0;i<sizeof(ref_r->gpr)/sizeof(word_t);i++){
 		if(ref_r->gpr[i]!=cpu.gpr[i])return false;
 	}
-  return true;
+  return ref_r->pc==pc;
 }
 
 void isa_difftest_attach() {
