6a8420c7d472a4bb261e4d04a5e483bc196edff5
tracer-ysyx <tracer@ysyx.org>
2025-11-11 16:58:08 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:58:08 up 3 days, 22:32,  1 user,  load average: 1.03, 1.10, 1.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -244,7 +244,7 @@ int callst_cnt=0;
 
 static void ftrace_log(const char* hint_str,word_t pc,const char* func_name,word_t func_addr){
 #ifdef CONFIG_FTRACE
-	printf("0x%08X:%-8s "ANSI_FMT("f`%08X",ANSI_FG_GRAY)"%*s%s \n",pc,hint_str,func_addr,callst_cnt,"",hint_str);
+	printf("0x%08X:%-8s "ANSI_FMT("f`%08X",ANSI_FG_GRAY)"%*s%s \n",pc,hint_str,func_addr,callst_cnt,"",func_name);
 #endif
 }
 
