d04c189f1442772b8732383de80d50b2a5a75f72
tracer-ysyx <tracer@ysyx.org>
2025-11-12 14:34:03 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:34:03 up  2:53,  1 user,  load average: 0.15, 0.13, 0.10

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -82,8 +82,10 @@ static inline void update_screen() {
 void vga_update_screen() {
   // call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  update_screen();
-  vgactl_port_base[1]=0;
+  if(vgactl_port_base[1]){
+	  update_screen();
+	  vgactl_port_base[1]=0;
+  }
 }
 
 void destroy_vga(){
