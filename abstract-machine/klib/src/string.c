5da5067ba54a20ee9a34ee0b217ebf62d1615629
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2025-11-12 11:39:44 +0800

    impl keyboard impl tmp malloc/free impl memmove impl dtrace

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -67,7 +67,10 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+	uint8_t* tmp=malloc(n);
+	memcpy(tmp, src, n);
+	memcpy(dst, tmp, n);
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
