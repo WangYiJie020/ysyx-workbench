107c66fc2fab63e75d6535c5414e9cd4e0a3b72f
tracer-ysyx <tracer@ysyx.org>
2025-11-12 15:01:10 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:01:10 up  3:19,  1 user,  load average: 0.08, 0.06, 0.08

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,8 +1,11 @@
 #include <am.h>
 #include <nemu.h>
 #include <stdio.h>
+#include <string.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+
+#define fb_as_u32 ((uint32_t *)(uintptr_t)FB_ADDR);
 
 void __am_gpu_init() {
   int i;
@@ -10,25 +13,38 @@ void __am_gpu_init() {
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
   printf("get w=%d h=%d\n",w,h);
+  uint32_t* fb=fb_as_u32;
  
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
+// AM假设系统在运行过程中, 屏幕大小不会发生变化.
+static uint16_t gpu_w,gpu_h;
+
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 	uint32_t packed_wh=inl(VGACTL_ADDR);
-	uint16_t h=packed_wh&0xffff;
-	uint16_t w=packed_wh>>16;
+	gpu_h=packed_wh&0xffff;
+	gpu_w=packed_wh>>16;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
+    .width = gpu_w, .height = gpu_h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
+	uint32_t* row_beg=fb_as_u32;
+	row_beg+=ctl->x*gpu_w+ctl->y;
+	uint32_t* row_end=row_beg+ctl->h*gpu_w;
+	uint32_t* pix=ctl->pixels;
+	while (row_beg!=row_end) {
+		memcpy(row_beg, pix, ctl->w);
+		pix+=ctl->w;
+		row_beg+=gpu_w;
+	}
+
     outl(SYNC_ADDR, 1);
   }
 }
