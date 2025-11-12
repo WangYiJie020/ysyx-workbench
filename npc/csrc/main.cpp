91d3b08a61f850ed7972f862280f871ad7ce1328
tracer-ysyx <tracer@ysyx.org>
2025-11-12 19:04:52 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:04:52 up  6:23,  1 user,  load average: 0.02, 0.04, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -24,7 +24,7 @@ typedef uint32_t addr_t;
 
 word_t guest_to_host(word_t addr){
 	word_t res= addr - MADDR_BASE;
-	//printf("raw addr %08X after trans: %08X\n",addr,res);
+	printf("raw addr %08X after trans: %08X\n",addr,res);
 	return res;
 }
 
