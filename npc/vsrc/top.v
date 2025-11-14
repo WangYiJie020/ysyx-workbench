bd41cb8c75468a013d3869ddef7dd14a7e3db866
tracer-ysyx <tracer@ysyx.org>
2025-11-14 19:29:51 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:29:51 up 1 day, 11:53,  1 user,  load average: 0.01, 0.04, 0.03

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -165,10 +165,10 @@ end
             default:;
 
         endcase
-//        $display("pc %08X nxt_pc %08X",pc,nxt_pc);
+        $display("pc %08X nxt_pc %08X",pc,nxt_pc);
     end
 
-    `define DISPLAY_TRACE
+    //`define DISPLAY_TRACE
 
     always@(posedge clk,posedge rst)begin
 
