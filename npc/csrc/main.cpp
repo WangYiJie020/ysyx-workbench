6ac6a2e5836e9d1e74b253468ee202a4890fd822
tracer-ysyx <tracer@ysyx.org>
2025-11-14 23:31:36 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:31:36 up 1 day, 14:17,  1 user,  load average: 0.83, 0.44, 0.29

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -50,7 +50,6 @@ void nvboard_init(int vga_clk_cycle);
 word_t mem[512*1024/4]={
   0x00000297,  // auipc t0,0
   0x00028823,  // sb  zero,16(t0)
-  0x00028823,  // sb  zero,16(t0)
   0x0102c503,  // lbu a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
