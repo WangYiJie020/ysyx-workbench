5fd2a8ad7377a81718d9be646232048336556444
tracer-ysyx <tracer@ysyx.org>
2025-11-15 14:28:40 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:28:40 up 1 day, 19:21,  1 user,  load average: 0.23, 0.13, 0.05

--- npc/csrc/sdb/elf_tool.hpp
+++ npc/csrc/sdb/elf_tool.hpp
@@ -1,16 +1,29 @@
 #pragma once
-#include <elf.h>
 #include <fstream>
+#include <elf.h>
+#include <vector>
+#include <string>
 
 class ElfHandler {
-	void loadElf();
-	void freeElf();
 public:
-		ElfHandler(std::fstream fs) {
-			loadElf();
-		}
-		~ElfHandler() {
-			freeElf();
-		}
+	struct FuncSym {
+		uint32_t addr;
+		uint32_t size;
+		std::string_view name;
+	};
+private:
+	std::fstream& _fs;
+	std::vector<FuncSym> _func_syms;
+	std::string _symstr_buf;
+	void loadElf();
+
+	void _ensure_frd(void* ptr, size_t siz);
+	std::string create_strbuf(const Elf32_Shdr& shdr);
+public:
+
+	ElfHandler(std::fstream& fs):_fs(fs) {
+		loadElf();
+	}
+	void dump_func_syms();
 
 };
