36a5d7708881089725182d16ef6391ec39d172d1
tracer-ysyx <tracer@ysyx.org>
2025-11-17 01:05:45 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:05:45 up 2 days, 14:50,  1 user,  load average: 0.03, 0.09, 0.08

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,5 +1,5 @@
 #include <am.h>
-
+#include <time.h>
 #define RTC_ADDR 0x10000048
 
 static uint64_t _am_start_time;
@@ -19,10 +19,21 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+	/*
+	uint64_t t=get_us_time();
+	time_t sec=t/1000000;
+	struct tm *tm_info=gmtime(&sec);
+	rtc->second = tm_info->tm_sec;
+	rtc->minute = tm_info->tm_min;
+	rtc->hour   = tm_info->tm_hour;
+	rtc->day    = tm_info->tm_mday;
+	rtc->month  = tm_info->tm_mon + 1;
+	rtc->year   = tm_info->tm_year + 1900;
+	*/
+	rtc->second = 0;
+	rtc->minute = 0;
+	rtc->hour   = 0;
+	rtc->day    = 0;
+	rtc->month  = 0;
+	rtc->year   = 1900;
 }
