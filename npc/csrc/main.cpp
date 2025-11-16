6437e7416cb8f60532d35e70b67c329262602026
tracer-ysyx <tracer@ysyx.org>
2025-11-16 23:50:11 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:50:11 up 2 days, 13:35,  1 user,  load average: 0.50, 0.14, 0.04

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -100,11 +100,11 @@ extern "C" int fetch_inst(int pc){
 }
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
+	printf("pmem_write called %08X\n",waddr);
 	if(waddr==MMIO_SERIAL_IO){
 		putchar(wdata&0xff);
 		return;
 	}
-	// printf("pmem_write called %08X\n",waddr);
 	// 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
 	// `wmask`中每比特表示`wdata`中1个字节的掩码,
 	// 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
