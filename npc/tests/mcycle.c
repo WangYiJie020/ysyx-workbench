63afc2f25ded3f489c4c4a2a7adaf3a8d16988db
tracer-ysyx <tracer@ysyx.org>
2025-11-20 21:46:59 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:46:59 up 4 days, 16:56,  1 user,  load average: 0.26, 0.14, 0.05

--- npc/tests/mcycle.c
+++ npc/tests/mcycle.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 void print(){
-	uint64_t c1,c2,c3;
+	uint32_t c1,c2,c3;
 	asm volatile ("csrr %0, mcycle" : "=r"(c1));
 	asm volatile ("csrr %0, mcycle" : "=r"(c2));
 	asm volatile ("csrr %0, mcycle" : "=r"(c3));
