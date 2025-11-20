b885805bdeb2fd91ef76d319ee5dcf055a3948fe
tracer-ysyx <tracer@ysyx.org>
2025-11-21 00:16:55 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:16:55 up 4 days, 18:31,  1 user,  load average: 0.15, 0.06, 0.01

--- npc/vsrc/ControlStatusRegister.v
+++ npc/vsrc/ControlStatusRegister.v
@@ -1,6 +1,10 @@
 parameter
     MCYCLE_ADDR = 12'hB00,
-    MCYCLEH_ADDR= 12'hB80;
+    MCYCLEH_ADDR= 12'hB80,
+    MSTATUS_ADDR= 12'h300,
+    MEPC_ADDR   = 12'h341,
+    MCAUSE_ADDR = 12'h342,
+    MTVEC_ADDR  = 12'h305;
 
 parameter MVENDORID_ADDR = 12'hF11;
 parameter MARCHID_ADDR   = 12'hF12;
@@ -10,17 +14,24 @@ parameter MARCHID_VALUE   = 32'd25100261;
 
 module ControlStatusRegister(
     input clk,
+    input [31:0] pc,
+    input [31:0] inst,
     input [11:0] addr,
     input [31:0] wdata,
     input wen,ren,
     output reg [31:0] rdata
 );
     reg [31:0] rf[5];
+
     reg [63:0] mcycle;
 
     reg [2:0] idx;
     always@(*) begin case(addr)
-        default:        idx = 3'd4; // unused
+        MSTATUS_ADDR: idx = 3'd0;
+        MEPC_ADDR:    idx = 3'd1;
+        MCAUSE_ADDR:  idx = 3'd2;
+        MTVEC_ADDR:   idx = 3'd3;
+        default:      idx = 3'd4; // unused
     endcase end
 
     always@(*) if(ren) begin
@@ -29,7 +40,7 @@ module ControlStatusRegister(
             MCYCLEH_ADDR:   rdata = mcycle[63:32];
             MVENDORID_ADDR: rdata = MVENDORID_VALUE;
             MARCHID_ADDR:   rdata = MARCHID_VALUE;
-            default:        rdata = 32'd0;
+            default:        rdata = rf[idx];
         endcase
     end else begin
         rdata = 32'd0;
@@ -37,6 +48,11 @@ module ControlStatusRegister(
 
     always@(posedge clk) begin
         mcycle <= mcycle + 1;
-
+        if(inst==INST_ECALL) begin
+            rf[1] <= pc; // mepc
+            rf[2] <= 32'd11; // mcause, environment call from M-mode
+        end else begin
+            if(wen) rf[idx] <= wdata;
+        end
     end
 endmodule
