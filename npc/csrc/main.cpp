7eaad24e6e038f0fba4ddcc5828b29e728fd451b
tracer-ysyx <tracer@ysyx.org>
2025-11-21 11:53:36 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:53:36 up 4 days, 22:28,  1 user,  load average: 0.24, 0.18, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -208,6 +208,7 @@ extern "C" int pmem_read(int raddr) {
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
 	if(waddr==MMIO_SERIAL_PORT){
+		printf("pmem_write to serial port: %c\n",wdata&0xff);
 		dbg.difftest_ref_skip();		
 		putchar(wdata&0xff);
 		return;
