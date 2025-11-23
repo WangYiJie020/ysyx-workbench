4f9ea6f815c8c6d9b369d8760853ca5ac27738a7
tracer-ysyx <tracer@ysyx.org>
2025-11-24 02:01:07 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:01:07 up  6:13,  1 user,  load average: 0.32, 0.14, 0.11

--- sdb/src/sdb.cpp
+++ sdb/src/sdb.cpp
@@ -85,6 +85,7 @@ void debuger::_step(size_t n){
 	for(size_t i=0;i<n&&is_running();i++){
 		ranges::for_each(before_exec,invoke);
 		_step_one();
+		if(!is_running())break;
 		ranges::for_each(after_exec,invoke);
 	}
 }
