768c874bf9615ed03e70933c48ad39391b70157b
tracer-ysyx <tracer@ysyx.org>
2025-11-23 19:35:47 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:35:47 up 6 min,  1 user,  load average: 0.09, 0.32, 0.21

--- sdb/src/expr.cpp
+++ sdb/src/expr.cpp
@@ -107,7 +107,7 @@ static bool make_token(char *e) {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
 			//printf("try match %s ,re %s\n",e+position,rules[i].regex);
-			if(regex_match(e+position,match_result,re[i])){
+			if(regex_search(e+position,match_result,re[i])){
         char *substr_start = e + position;
 				
         int substr_len = match_result[0].length();
