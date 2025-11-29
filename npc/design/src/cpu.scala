34edd53fc2b3e85b228ec6798149cfa36f6f1058
tracer-ysyx <tracer@ysyx.org>
2025-11-29 17:47:50 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:47:50 up 2 days, 13:31,  1 user,  load average: 0.00, 0.18, 0.38

--- npc/design/src/cpu.scala
+++ npc/design/src/cpu.scala
@@ -56,7 +56,7 @@ class IFU extends Module {
   }
   val fooitem=Wire(UInt(32.W))
   fooitem:=0.U
-  val res = RawUnclockedNonVoidFunctionCall("foosdhkjh",UInt(32.W),Some(Seq("pc")))(1.B,fooitem)
+  val res = RawUnclockedNonVoidFunctionCall("foosdhkjh",UInt(32.W))(1.B,fooitem)
 
 }
 
