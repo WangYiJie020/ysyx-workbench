5470d9c3768985e5dee23740912e3ffce3fc9afa
tracer-ysyx <tracer@ysyx.org>
2025-12-03 14:42:31 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:42:31 up 4 days, 15:28,  1 user,  load average: 0.21, 0.10, 0.15

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -1,9 +1,13 @@
 #include <VTop.h>
 #include <VTop__Dpi.h>
+#include <array>
 #include <cstdint>
 #include <cstdio>
-#include <iostream>
 #include <nvboard.h>
+#include <string_view>
+
+#include "sdb.hpp"
+#include "tracers.hpp"
 
 #ifndef TOP_NAME
 #endif
@@ -11,41 +15,43 @@ TOP_NAME dut;
 void nvboard_bind_all_pins(TOP_NAME *top);
 
 static void step_cycle() {
-//	printf("-----step-----\n");
-	dut.clock = 0;
-	dut.eval();
+  //	printf("-----step-----\n");
+  dut.clock = 0;
+  dut.eval();
 
-	dut.clock = 1;
-	dut.eval();
+  dut.clock = 1;
+  dut.eval();
 
-//	printf("-------------\n");
+  //	printf("-------------\n");
 }
 static void reset(int n) {
-	dut.reset = 1;
-	while (n-- > 0) {
-		step_cycle();
-	}
-	dut.reset = 0;
+  dut.reset = 1;
+  while (n-- > 0) {
+    step_cycle();
+  }
+  dut.reset = 0;
 }
 
 static bool is_running = true;
 void raise_ebreak() {
-	printf("ebreak raised, stop sim\n");
-	is_running = false;
+  printf("ebreak raised, stop sim\n");
+  is_running = false;
 }
 
 typedef uint32_t word_t;
 typedef uint32_t addr_t;
 
 #define MADDR_BASE 0x80000000u
+#define INITIAL_PC MADDR_BASE
+
 word_t mem[600 * 1024 * 1024 / 4] = {
     0x00000297, // auipc t0,0
-    //0x00028823, // sb  zero,16(t0)
+    // 0x00028823, // sb  zero,16(t0)
     0x0102c503, // lbu a0,16(t0)
     0x0102c503, // lbu a0,16(t0)
     0x00100073, // ebreak
     0xdeadbeef, // some data
-		0x12345678,
+    0x12345678,
 };
 uint8_t *mem_atguest(size_t addr) {
   assert(addr >= MADDR_BASE);
@@ -60,61 +66,113 @@ word_t guest_to_host(word_t addr) {
 
 word_t gpr_snap[32];
 void gpr_upd(int regno, int data) {
-	if (regno == 0)
-		return;
-	gpr_snap[regno] = data;
+  if (regno == 0)
+    return;
+  gpr_snap[regno] = data;
 }
+std::array<std::string_view, 32> reg_names = {
+    "$0", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
+    "a1", "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
+    "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 bool pc_changed = false;
+word_t current_pc = INITIAL_PC;
 void pc_upd(int pc, int npc) {
-//	printf("pc upd pc=%08x npc=%08x\n",pc,npc);
-	pc_changed = true;
+  //	printf("pc upd pc=%08x npc=%08x\n",pc,npc);
+  pc_changed = true;
+  current_pc = npc;
 }
 
 void fetch_inst(int pc, int *out_inst) {
-	printf("fetch pc=%08x\n",pc);
-	*out_inst= mem[guest_to_host(pc) / 4];
+  printf("fetch pc=%08x\n", pc);
+  *out_inst = mem[guest_to_host(pc) / 4];
 }
 void pmem_read(int addr, int *out_data) {
-	uint32_t host_aligned = guest_to_host(addr)&(~0x3);
-	*out_data = mem[host_aligned/4];
-	printf("pmem read addr=%08x get %08X\n",addr,*out_data);
+  uint32_t host_aligned = guest_to_host(addr) & (~0x3);
+  *out_data = mem[host_aligned / 4];
+  printf("pmem read addr=%08x get %08X\n", addr, *out_data);
 }
 void pmem_write(int addr, int data, int mask) {
-	printf("pmem write addr=%08x data=%08x mask=%02x\n",addr,data,mask);
-	uint32_t host_aligned = guest_to_host(addr)&(~0x3);
-	
-	uint8_t* p=(uint8_t*)(&mem[host_aligned>>2]);
-	uint32_t umask=mask,udata=data;
+  printf("pmem write addr=%08x data=%08x mask=%02x\n", addr, data, mask);
+  uint32_t host_aligned = guest_to_host(addr) & (~0x3);
 
-	while (umask) {
-		if(umask&1){
-			*p=udata&0xff;
-		}
-		p++;
-		umask>>=1;
-		udata>>=8;
-	}
+  uint8_t *p = (uint8_t *)(&mem[host_aligned >> 2]);
+  uint32_t umask = mask, udata = data;
+
+  while (umask) {
+    if (umask & 1) {
+      *p = udata & 0xff;
+    }
+    p++;
+    umask >>= 1;
+    udata >>= 8;
+  }
 }
+
+void step_inst() {
+  while (!pc_changed) {
+    step_cycle();
+  }
+  pc_changed = false;
+}
+
+// SDB
+
+namespace sdbwrap {
+sdb::paddr_t cpu_exec(size_t n) {
+  while (n-- > 0) {
+    step_inst();
+  }
+  return current_pc;
+}
+void shot_regsnap(sdb::reg_snapshot_t &regsnap) {
+  for (size_t i = 0; i < 32; i++) {
+    regsnap[i] = gpr_snap[i];
+  }
+}
+sdb::vlen_inst_code inst_fetcher(sdb::paddr_t pc) {
+  word_t inst;
+  fetch_inst(pc, (int *)&inst);
+	uint8_t *p = (uint8_t *)&inst;
+	return sdb::vlen_inst_code(p, p + 4);
+}
+uint8_t *loadmem(sdb::paddr_t addr, size_t nbyte) { return mem_atguest(addr); }
+} // namespace sdbwrap
+
+std::shared_ptr<sdb::debuger> dbg;
+sdb::difftest_trace_handler_ptr diff_handler;
 
 int main() {
   nvboard_bind_all_pins(&dut);
-  //nvboard_init();
-	
-	reset(10);
+  // nvboard_init();
 
-  while (is_running) {
-		step_cycle();
-		if(pc_changed){
-			printf("inst finished execution, after pc changed, gprs:\n");
-			pc_changed=false;
-			printf("gpr snapshot:\n");
-			for(int i=0;i<32;i++){
-				printf("x%02d=%08x ",i,gpr_snap[i]);
-				if(i%8==7)printf("\n");
-			}
-		}
-    //nvboard_update();
+  size_t img_size = sizeof(mem);
+
+  dbg = std::make_shared<sdb::debuger>(
+      INITIAL_PC, INITIAL_PC, img_size, sdbwrap::cpu_exec, sdbwrap::loadmem,
+      sdbwrap::shot_regsnap,
+      std::vector<std::string_view>(reg_names.begin(), reg_names.end()),
+      sdbwrap::inst_fetcher);
+
+  dbg->enable_inst_trace = true;
+
+  dbg->add_trace(sdb::make_disasm_trace_handler(sdb::default_inst_disasm, 16));
+  dbg->add_trace(sdb::make_etrace_handler());
+  dbg->add_trace(sdb::make_iringbuf_trace_handler());
+
+  diff_handler = sdb::make_difftest_trace_handler(
+      "../nemu/build/riscv32-nemu-interpreter-so", 0);
+  dbg->add_trace(diff_handler);
+  reset(10);
+
+  std::string cmd;
+  while (true) {
+    std::cout << "(sdb) ";
+    std::getline(std::cin, cmd);
+    dbg->exec_command(cmd);
+    if (dbg->state().state == sdb::run_state::quit) {
+      break;
+    }
   }
   return 0;
 }
