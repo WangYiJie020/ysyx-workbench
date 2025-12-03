387f7934ce292a7f81fe4091268c03be74fffa7b
tracer-ysyx <tracer@ysyx.org>
2025-12-03 19:44:24 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:44:24 up 4 days, 20:24,  1 user,  load average: 0.18, 0.22, 0.29

--- npc/design/src/cpu.scala
+++ npc/design/src/cpu.scala
@@ -94,14 +94,24 @@ class IFU extends Module {
   val fsm = Module(new OneMasterOneSlaveFSM)
   fsm.connectMaster(io.pc)
   fsm.connectSlave(io.out)
-  fsm.io.self_finished := true.B
+
+  val valid_pc = RegInit(false.B)
+  when(fsm.io.master_valid) {
+    valid_pc := true.B
+  }.elsewhen(fsm.io.slave_ready) {
+    valid_pc := false.B
+  }
+
+  fsm.io.self_finished := valid_pc
 
   printf("(ifu) fetch inst %x at pc 0x%x\n", io.out.bits.code,io.pc.bits)
   printf("(ifu) enable: %b\n", io.pc.valid)
 
+  val code= RawClockedNonVoidFunctionCall("fetch_inst", Types.UWord)(clock, valid_pc, io.pc.bits)
+
   // NOTICE: dpi function auto generated with void return
   // see https://github.com/llvm/circt/blob/main/docs/Dialects/FIRRTL/FIRRTLIntrinsics.md#dpi-intrinsic-abi
-  io.out.bits.code := RawClockedNonVoidFunctionCall("fetch_inst", Types.UWord)(clock, true.B, io.pc.bits)
+  io.out.bits.code := code
   io.out.bits.pc   := io.pc.bits
 }
 
