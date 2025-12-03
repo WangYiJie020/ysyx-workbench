41baacaad23b4cc1bd85d1416307f2be4b34cffb
tracer-ysyx <tracer@ysyx.org>
2025-12-03 13:46:45 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:46:45 up 4 days, 14:33,  1 user,  load average: 0.06, 0.27, 0.24

--- npc/design/src/cpu.scala
+++ npc/design/src/cpu.scala
@@ -440,12 +440,24 @@ class EXU           extends Module {
   val mem_addr_unalign_part_bitlen = mem_addr_unalign_part << 3
 
   val mem_raddr     = io.mem_rreq.addr
-  val mem_raw_rdata = io.mem_rreq.data
+//  val mem_raw_rdata = io.mem_rreq.data
+
+  val mem_raw_rdata = Reg(Types.UWord)
+  val mem_read_responded = RegInit(false.B)
+  
   val mem_ren       = io.mem_rreq.en
 
   val mem_data = mem_raw_rdata >> mem_addr_unalign_part_bitlen
 
-  mem_ren   := dinst.info.typ === InstType.load
+  mem_ren   := (dinst.info.typ === InstType.load)&&(!mem_read_responded)
+
+  when(io.mem_rreq.respValid) {
+    mem_raw_rdata      := io.mem_rreq.data
+    mem_read_responded := true.B
+  }.elsewhen(mem_ren) {
+    mem_read_responded := false.B
+  }
+
   mem_raddr := mem_addr
 
 //when(mem_ren) {
