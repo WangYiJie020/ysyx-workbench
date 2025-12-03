9893229ac518a9f48ac81bb1216ac5a1152e54d6
tracer-ysyx <tracer@ysyx.org>
2025-12-03 18:32:28 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:32:28 up 4 days, 19:14,  1 user,  load average: 0.72, 0.47, 0.40

--- npc/design/src/memory.scala
+++ npc/design/src/memory.scala
@@ -24,7 +24,6 @@ object MemReqIO {
     val mask = Input(UInt(4.W))
     val en   = Input(Bool())
 
-
   }
 
   def ReadRX  = new _ReadRX
@@ -39,18 +38,19 @@ class MemUnit extends Module {
     val write = MemReqIO.WriteRX
   })
 
-  when(io.read.en){
-    //printf("(MemUnit) read enabled addr: 0x%x\n", io.read.addr)
+  when(io.read.en) {
+    // printf("(MemUnit) read enabled addr: 0x%x\n", io.read.addr)
   }
-  //printf("(MemUnit) write en: %b addr: 0x%x data: 0x%x mask: 0b%b\n", io.write.en, io.write.addr, io.write.data, io.write.mask)
+  // printf("(MemUnit) write en: %b addr: 0x%x data: 0x%x mask: 0b%b\n", io.write.en, io.write.addr, io.write.data, io.write.mask)
 
-
-  io.read.data := RawClockedNonVoidFunctionCall("pmem_read", Types.UWord)(
+  val data = Reg(Types.UWord)
+  data := RawClockedNonVoidFunctionCall("pmem_read", Types.UWord)(
     clock,
-    io.read.en&&(!reset.asBool),
+    io.read.en && (!reset.asBool),
     io.read.addr
   )
 
+  io.read.data      := data
   io.read.respValid := io.read.en
 
   RawClockedVoidFunctionCall("pmem_write")(
