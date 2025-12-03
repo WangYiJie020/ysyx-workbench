cbd41368e10a7b808d0de2f01de3e16290011ca2
tracer-ysyx <tracer@ysyx.org>
2025-12-03 19:26:08 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:26:08 up 4 days, 20:07,  1 user,  load average: 0.19, 0.45, 0.40

--- npc/design/src/cpu.scala
+++ npc/design/src/cpu.scala
@@ -96,13 +96,13 @@ class IFU extends Module {
   fsm.connectSlave(io.out)
   fsm.io.self_finished := true.B
 
-  // printf("(ifu) fetch inst at pc 0x%x\n", io.pc.bits)
-  // printf("(ifu) enable: %b\n", io.pc.valid)
+ printf("(ifu) fetch inst at pc 0x%x\n", io.pc.bits)
+ printf("(ifu) enable: %b\n", io.pc.valid)
 
-//  printf("(ifu) fsm st %d out.valid %b\n",fsm.io._state, io.out.valid)
-//  when(io.out.ready){
-//    stop()
-//  }
+  printf("(ifu) fsm st %d out.valid %b\n",fsm.io._state, io.out.valid)
+  when(io.out.ready){
+    stop()
+  }
 
   // NOTICE: dpi function auto generated with void return
   // see https://github.com/llvm/circt/blob/main/docs/Dialects/FIRRTL/FIRRTLIntrinsics.md#dpi-intrinsic-abi
@@ -320,6 +320,8 @@ class EXU           extends Module {
   val MS_fsm = Module(new OneMasterOneSlaveFSM)
   MS_fsm.connectMaster(io.dinst)
   MS_fsm.connectSlave(io.out)
+
+  printf("(exu) inst 0x%x at pc 0x%x\n", dinst.code, dinst.pc)
 
   /*
   printf("(exu) fsm st %d alu.valid %b mem_rreq.respValid %b\n",MS_fsm.io._state, alu.io.out.valid, io.mem_rreq.respValid)
