17a88d4903f4aabff4a2d6d3e3ab23376431789f
tracer-ysyx <tracer@ysyx.org>
2025-12-03 20:14:22 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:14:22 up 4 days, 20:54,  1 user,  load average: 0.26, 0.39, 0.39

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -293,7 +293,7 @@ int main(int argc, char **argv) {
 
   dbg->enable_inst_trace = true;
 
-  dbg->add_trace(sdb::make_disasm_trace_handler(sdb::default_inst_disasm, -1));
+  dbg->add_trace(sdb::make_disasm_trace_handler(sdb::default_inst_disasm, 16));
   dbg->add_trace(sdb::make_etrace_handler());
   dbg->add_trace(sdb::make_iringbuf_trace_handler());
 
