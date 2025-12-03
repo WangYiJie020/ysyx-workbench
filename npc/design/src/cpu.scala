6f081f09be18e847d38d450abeb8ae4548b14fce
tracer-ysyx <tracer@ysyx.org>
2025-12-03 18:10:55 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:10:55 up 4 days, 18:53,  1 user,  load average: 0.07, 0.33, 0.42

--- npc/design/src/cpu.scala
+++ npc/design/src/cpu.scala
@@ -106,7 +106,10 @@ class IFU extends Module {
 
   // NOTICE: dpi function auto generated with void return
   // see https://github.com/llvm/circt/blob/main/docs/Dialects/FIRRTL/FIRRTLIntrinsics.md#dpi-intrinsic-abi
-  io.out.bits.code := RawClockedNonVoidFunctionCall("fetch_inst", Types.UWord)(clock, io.pc.valid, io.pc.bits)
+  val inst=RawClockedNonVoidFunctionCall("fetch_inst", Types.UWord)(clock, io.pc.valid, io.pc.bits)
+  printf("(ifu) dpi fetch_inst return 0x%x\n", inst)
+
+  io.out.bits.code := inst
   io.out.bits.pc   := io.pc.bits
 
   printf("(ifu) fetched inst 0x%x at pc 0x%x\n", io.out.bits.code, io.out.bits.pc)
