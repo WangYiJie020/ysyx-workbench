f1f5222220b72d46dadce508ea4bb9984ef65f00
tracer-ysyx <tracer@ysyx.org>
2025-12-04 11:55:06 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:55:06 up 5 days,  4:31,  1 user,  load average: 0.82, 0.64, 0.37

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -259,17 +259,20 @@ static void parse_args(int argc, char **argv) {
 }
 
 void read_and_check() {
-		vpiHandle vh1 = vpi_handle_by_name((PLI_BYTE8*) ("TOP.Top._wbu_io_data_ready"), NULL);
-		if (!vh1) vl_fatal(__FILE__, __LINE__, "sim_main", "No handle found");
-		const char* name = vpi_get_str(vpiName, vh1);
-		const char* type = vpi_get_str(vpiType, vh1);
-		const int size = vpi_get(vpiSize, vh1);
-		printf("register name: %s, type: %s, size: %d\n", name, type, size);
+  vpiHandle vh1 =
+      vpi_handle_by_name((PLI_BYTE8 *)("TOP.Top._wbu_io_data_ready"), NULL);
+  if (!vh1)
+    vl_fatal(__FILE__, __LINE__, "sim_main", "No handle found");
+  const char *name = vpi_get_str(vpiName, vh1);
+  const char *type = vpi_get_str(vpiType, vh1);
+  const int size = vpi_get(vpiSize, vh1);
+  printf("register name: %s, type: %s, size: %d\n", name, type, size);
 
-		s_vpi_value v;
-		v.format = vpiIntVal;
-		vpi_get_value(vh1, &v);
-		printf("Value of %s: %d\n", name, v.value.integer);  // Prints "Value of readme: 0"
+  s_vpi_value v;
+  v.format = vpiIntVal;
+  vpi_get_value(vh1, &v);
+  printf("Value of %s: %d\n", name,
+         v.value.integer); // Prints "Value of readme: 0"
 }
 
 // SDB
@@ -345,12 +348,17 @@ int main(int argc, char **argv) {
     return dbg->state().is_badexit();
   }
 
+  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+  const std::unique_ptr<TOP_NAME> top{new TOP_NAME{contextp.get()}};
+
+  contextp->internalsDump(); // See scopes to help debug
+														 
   std::string cmd;
   while (true) {
     std::cout << "(sdb) ";
     std::getline(std::cin, cmd);
     dbg->exec_command(cmd);
-		read_and_check();
+    read_and_check();
     if (dbg->state().state == sdb::run_state::quit) {
       break;
     }
