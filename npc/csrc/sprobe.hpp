e252ce57161300575f444bc10235c626022ee724
tracer-ysyx <tracer@ysyx.org>
2025-12-05 00:13:48 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:13:48 up 5 days, 14:51,  1 user,  load average: 0.52, 0.29, 0.21

--- npc/csrc/sprobe.hpp
+++ npc/csrc/sprobe.hpp
@@ -74,7 +74,7 @@ public:
 #define ANSIFMT_NUM "\e[38;2;181;206;168m"
 #define ANSIFMT_SIGNAL_TYPE "\e[38;2;78;201;176m"
 
-    std::cout << ANSIFMT_GRAY << "Signal Probe";
+    std::cout << ANSIFMT_GRAY << "Signal Probe"<<ANSIFMT_NONE << std::endl;
 
     for (auto &h : _watched_handles) {
       s_vpi_value v;
