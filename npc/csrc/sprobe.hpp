78bb90be1a00659bf21d85164468f597f43f1b42
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2025-12-05 00:01:48 +0800

    make output colorful

--- npc/csrc/sprobe.hpp
+++ npc/csrc/sprobe.hpp
@@ -53,6 +53,13 @@ public:
                 << std::endl;
       return false;
     }
+    auto type = vpi_get(vpiType, vh1);
+    if (type == vpiModule) {
+      std::cout << "SProbe add_watch cannot watch module " << sig_name
+                << std::endl;
+      vpi_release_handle(vh1);
+      return false;
+    }
     _watched_handles.push_back(vh1);
     return true;
   }
@@ -60,16 +67,39 @@ public:
   void dump_watched() {
     if (_watched_handles.empty())
       return;
+
+#define ANSIFMT_NONE "\e[0m"
+#define ANSIFMT_GRAY "\e[38;2;90;90;90m"
+#define ANSIFMT_SIGNAL_NAME "\e[38;2;156;220;254m"
+#define ANSIFMT_NUM "\e[38;2;181;206;168m"
+#define ANSIFMT_SIGNAL_TYPE "\e[38;2;78;201;176m"
+
     // std::cout << "===== SProbe Watched Signals =====" << std::endl;
     for (auto &h : _watched_handles) {
       s_vpi_value v;
       v.format = vpiIntVal;
       vpi_get_value(h, &v);
-      std::string fullname = vpi_get_str(vpiFullName, h);
-      std::cout << std::format("{} ` {} [{}] = {:08X}\n",
-					vpi_get_str(vpiType, h), fullname,
-					vpi_get(vpiSize, h)
-					, (uint32_t)v.value.integer);
+      std::string_view fullname = vpi_get_str(vpiFullName, h);
+      std::string_view type = vpi_get_str(vpiType, h);
+      auto sig_width = vpi_get(vpiSize, h);
+      if (type.starts_with("vpi")) {
+        type = type.substr(3);
+      }
+      // Remove the "TOP."
+      auto notop_name = fullname.substr(4);
+      if (notop_name.starts_with("Top.")) {
+        notop_name = notop_name.substr(4);
+      }
+
+      auto val_out_width = (sig_width + 3) / 4; // (8bits per 2hex) upceil
+
+      std::cout << std::format(
+          ANSIFMT_GRAY "Signal " ANSIFMT_NUM "{:2}W " ANSIFMT_SIGNAL_TYPE
+                       "{} " ANSIFMT_SIGNAL_NAME "{}" ANSIFMT_NONE
+                       " = " ANSIFMT_GRAY "h'" ANSIFMT_NUM
+                       "{:0{}x}\n" ANSIFMT_NONE,
+          sig_width, type, notop_name, (uint32_t)v.value.integer,
+          val_out_width);
     }
   }
 };
