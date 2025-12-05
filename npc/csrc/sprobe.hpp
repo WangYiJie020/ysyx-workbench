5618547b61ec59accc8fc95d292d442fd200ab37
tracer-ysyx <tracer@ysyx.org>
2025-12-05 22:25:04 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:25:04 up 6 days,  2:18,  1 user,  load average: 0.26, 0.23, 0.23

--- npc/csrc/sprobe.hpp
+++ npc/csrc/sprobe.hpp
@@ -31,7 +31,7 @@ public:
     }
     void updateLastValue() { last_value = getValue(); }
     std::string_view getFullname() { return vpi_get_str(vpiFullName, handle); }
-    std::string_view getType() { return vpi_get_str(vpiType, handle); }
+    std::string getType() { return vpi_get_str(vpiType, handle); }
     std::string_view getName() { return vpi_get_str(vpiName, handle); }
     auto getSize() { return vpi_get(vpiSize, handle); }
     WatchItem(vpiHandle h) : handle(h) { last_value = getValue(); }
