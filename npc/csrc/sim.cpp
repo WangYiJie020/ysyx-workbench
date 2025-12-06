2b7bcc05fe2645035d689d3c69daf95b177ad76a
tracer-ysyx <tracer@ysyx.org>
2025-12-06 16:29:06 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:29:06 up 6 days,  9:54,  1 user,  load average: 0.55, 0.89, 0.50

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -86,6 +86,7 @@ void raise_ebreak(int a0) {
 
 #define ANSI_FG_RED "\33[1;31m"
 #define ANSI_FG_GREEN "\33[1;32m"
+#define ANSI_FG_YELLOW "\33[1;33m"
 #define ANSI_NONE "\33[0m"
 
   if (a0 == 0) {
@@ -231,10 +232,11 @@ void step_inst() {
   while (!pc_changed) {
     sim_step_cycle();
     cyc_cnt++;
-    if (cyc_cnt > MAYBE_DEADLOOP_THRESHOLD) {
-      printf("sim has stepped %zu cycles without pc change, maybe deadloop "
-             "happened\n",
-             cyc_cnt);
+    if (cyc_cnt >= MAYBE_DEADLOOP_THRESHOLD) {
+      puts(ANSI_FG_YELLOW "[WARN]" ANSI_NONE);
+      printf(
+          "sim has stepped %zu cycles without pc change, maybe lock happened\n",
+          cyc_cnt);
       printf("wanting to continue? (y/n) ");
       char c = getchar();
       if (c == 'y' || c == 'Y') {
