cacf05715c4300af644df2f8b6bf36fe07f825f0
tracer-ysyx <tracer@ysyx.org>
2025-12-06 12:42:53 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:42:53 up 6 days,  6:08,  1 user,  load average: 0.22, 0.32, 0.34

--- npc/csrc/sprobe.hpp
+++ npc/csrc/sprobe.hpp
@@ -34,7 +34,8 @@ class SProbe {
       "_RANDOM",
   };
   bool _in_ignore_list(std::string_view name) {
-		if(name.starts_with("_GEN"))return true;
+    if (name.starts_with("_GEN"))
+      return true;
     for (auto &ign : _ignored_names) {
       if (ign == name)
         return true;
@@ -55,10 +56,10 @@ public:
       s_vpi_value v;
       v.format = vpiVectorVal;
       vpi_get_value(handle, &v);
-			uint64_t res=v.value.vector[1].aval;
-			res <<=32;
-			res |=v.value.vector[0].aval;
-			return res;
+      uint64_t res = v.value.vector[1].aval;
+      res <<= 32;
+      res |= v.value.vector[0].aval;
+      return res;
     }
     void updateLastValue() { last_value = getValue(); }
     WatchItem(vpiHandle h) {
@@ -99,7 +100,7 @@ public:
             if (_in_ignore_list(vpi_get_str(vpiName, it))) {
               std::cout << "ignore " << vpi_get_str(vpiType, it) << " "
                         << vpi_get_str(vpiFullName, it) << std::endl;
-							vpi_release_handle(it);
+              vpi_release_handle(it);
               continue;
             }
             std::cout << "add watch " << vpi_get_str(vpiType, it) << " "
@@ -132,77 +133,5 @@ public:
     return true;
   }
 
-  void dump_watched() {
-    if (_watched.empty())
-      return;
-
-    bool is_first = true;
-
-    std::cout << ANSIFMT_COMMENT << "-- poke beg\n" << ANSIFMT_NONE;
-
-    std::string last_name = "";
-    std::string showed_type;
-
-    auto val_upd_hint = ANSIFMT_HINT "*" ANSIFMT_NONE;
-
-    for (auto &h : _watched) {
-      const auto &fullname = h.fullname;
-      const auto &selfname = h.name;
-      const auto &type = h.type;
-      const auto &sig_width = h.width;
-
-      if (type.starts_with("vpi")) {
-        showed_type = type[3];
-      } else {
-        showed_type = type;
-      }
-
-      // Remove the "TOP."
-      auto cur_name = fullname.substr(4);
-      if (cur_name.starts_with("Top.")) {
-        cur_name = cur_name.substr(4);
-      }
-
-      auto sig_value = h.getValue();
-      bool value_changed = (sig_value != h.last_value);
-
-      auto common_prefix = _max_common_prefix(last_name, cur_name);
-      last_name = cur_name;
-
-			// prefix should end with '.' or '_' (module/level separator)
-			while(common_prefix.size()>0&&
-				(common_prefix.back()!='.'&&common_prefix.back()!='_')){
-				common_prefix=common_prefix.substr(0,common_prefix.size()-1);
-			}
-			// remove last '.' or '_', which should show in the unique part
-      if (!common_prefix.empty()){
-        common_prefix = common_prefix.substr(0, common_prefix.size() - 1);
-      }
-
-      auto unique_part = cur_name.substr(common_prefix.size());
-
-      std::string showed_name = std::format(
-          ANSIFMT_SIGNAL_REPEATED_PARENT "{}{}{}" ANSIFMT_NONE, common_prefix,
-          value_changed ? ANSIFMT_HINT ANSIFMT_BOLD : ANSIFMT_SIGNAL_NAME,
-          unique_part);
-
-      auto val_out_width = (sig_width + 3) / 4; // (8bits per 2hex) upceil
-
-      if (!is_first) {
-        std::cout << std::endl;
-      } else {
-        is_first = false;
-      }
-
-      std::cout << std::format(
-          ANSIFMT_GRAY "si{} " ANSIFMT_SIGNAL_TYPE "{} " ANSIFMT_NUM "{:2} "
-                       "{}" ANSIFMT_NONE " = " ANSIFMT_NUM_PREFIX
-                       "h'{}" ANSIFMT_NUM "{:0{}x}" ANSIFMT_NONE,
-          value_changed ? val_upd_hint : "g", showed_type, sig_width,
-          showed_name, value_changed ? ANSIFMT_BOLD : ANSIFMT_NONE,
-          (uint32_t)sig_value, val_out_width);
-      h.updateLastValue();
-    }
-    std::cout << ANSIFMT_COMMENT " -- end" ANSIFMT_NONE << std::endl;
-  }
+  void dump_watched();
 };
