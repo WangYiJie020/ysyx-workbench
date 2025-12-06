3c60bc26b88ecc4afb2280f3a8b5d5a1dfdcfedf
tracer-ysyx <tracer@ysyx.org>
2025-12-06 12:10:04 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:10:04 up 6 days,  5:35,  1 user,  load average: 0.48, 0.18, 0.12

--- npc/csrc/sprobe.hpp
+++ npc/csrc/sprobe.hpp
@@ -148,7 +148,7 @@ public:
       const auto &sig_width = h.width;
 
       if (type.starts_with("vpi")) {
-        showed_type = std::string(1,type[3]);
+        showed_type = type[3];
       } else {
         showed_type = type;
       }
@@ -163,6 +163,7 @@ public:
       bool value_changed = (sig_value != h.last_value);
 
       auto common_prefix = _max_common_prefix(last_name, cur_name);
+			printf("DEBUG: cur_name='%s' last_name='%s' common_prefix='%s'\n",cur_name.c_str(),last_name.c_str(),common_prefix.data());
       last_name = cur_name;
       auto common_prefix_back = common_prefix.back();
       if (common_prefix_back == '.' || common_prefix_back == '_') {
