76d411966db7aa08cad344c2a94f8700717c32c8
tracer-ysyx <tracer@ysyx.org>
2025-12-08 18:39:08 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:39:08 up  4:58,  1 user,  load average: 1.36, 0.63, 0.46

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
 	// setting.trace_clock_cycle=true;
 	setting.always_show_disasm=true;
 	setting.enable_inst_trace=true;
-	setting.trace_mmio_write=true;
+	// setting.trace_mmio_write=true;
 	setting.enable_waveform=false;
 
 	setting.cycle_finish_cb=cyc_callback;
