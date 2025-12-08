5fc38be08a888dc301f05a6a7f3789b945cc5216
tracer-ysyx <tracer@ysyx.org>
2025-12-08 18:56:49 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:56:49 up  5:14,  1 user,  load average: 0.81, 0.64, 0.58

--- npc/design/src/Top.scala
+++ npc/design/src/Top.scala
@@ -162,7 +162,7 @@ class Top(word_width: Int = 32) extends Module {
   val memXBar = Module(new AXI4LiteXBar(Seq(
     (MEM_BASE,MEM_END) -> mem.io,
     (SERIAL_BASE,SERIAL_END) -> uart.io,
-    // ("h10000048".U(32.W),"h10000050".U(32.W)) -> mem.io, // RTC
+    ("h10000048".U(32.W),"h10000050".U(32.W)) -> mem.io, // RTC
   )))
 
   memXBar.connect()
