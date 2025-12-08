539e6ecdd6935adb9afa122b88d4ba32b3cc2240
tracer-ysyx <tracer@ysyx.org>
2025-12-08 21:27:59 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:27:59 up  7:45,  1 user,  load average: 0.40, 0.48, 0.27

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -12,7 +12,7 @@ static uint64_t get_us_time() {
 void __am_timer_init() { _am_start_time = get_us_time(); }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = get_us_time() - _am_start_time;
+  uptime->us = (get_us_time() - _am_start_time)/2; // adjust to make npc timer closer to real time
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
