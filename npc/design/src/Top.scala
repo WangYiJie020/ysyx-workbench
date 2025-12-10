653995ef3c12364efe155d87d7cfc1c991e42589
tracer-ysyx <tracer@ysyx.org>
2025-12-10 14:24:11 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:24:11 up 1 day,  2:41,  1 user,  load average: 0.15, 0.50, 0.41

--- npc/design/src/Top.scala
+++ npc/design/src/Top.scala
@@ -177,8 +177,8 @@ class ysyx_25100261(word_width: Int = 32) extends Module {
   val memXBar = Module(new AXI4LiteXBar(Seq(
     // (MEM_BASE,MEM_END) -> mem.io,
     // (SERIAL_BASE,SERIAL_END) -> uart.io,
-    ("h10000048".U(32.W),"h10000050".U(32.W)) -> clint.io,
-    ("h20000000".U(32.W),"hffffffff".U(32.W)) -> otherReqSlave
+    // ("h10000048".U(32.W),"h10000050".U(32.W)) -> clint.io,
+    ("h10000000".U(32.W),"hffffffff".U(32.W)) -> otherReqSlave
   )))
 
   AXI4IO.connectMasterSlave(memArbiter.io.out, memXBar.io.in)
