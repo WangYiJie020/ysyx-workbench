f04223323a33caef9841cb655826292001f072b3
tracer-ysyx <tracer@ysyx.org>
2025-12-11 19:03:59 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:03:59 up 1 day, 22:39,  1 user,  load average: 0.22, 0.13, 0.16

--- sdb/src/ftrace.cpp
+++ sdb/src/ftrace.cpp
@@ -1,74 +1,91 @@
-#include <tracers.hpp>
 #include "ansi_col.h"
 #include "elf_tool.hpp"
+#include "sdb.hpp"
+#include <tracers.hpp>
 
 using namespace std;
 using namespace sdb;
 
-jump_type sdb::default_riscv_jump_recognizer(vlen_inst_view inst){
-	word_t instr=*(word_t*)inst.data();
-	uint8_t opcode=instr&0x7f;
-	uint8_t funct3=(instr>>12)&0x7;
-	uint8_t rd=(instr>>7)&0x1f;
-	uint8_t rs1=(instr>>15)&0x1f;
-	bool is_jal=opcode==0x6f;
-	bool is_jalr=opcode==0x67 && funct3==0x0;
-	bool is_ret=is_jalr && rd==0 && rs1==1;
-	bool is_call=(is_jal && rd==1) || (is_jalr && rd==1);
-	if(is_call)return jump_type::call;
-	if(is_ret)return jump_type::ret;
-	return jump_type::normal;
+jump_type sdb::default_riscv_jump_recognizer(vlen_inst_view inst) {
+  word_t instr = *(word_t *)inst.data();
+  uint8_t opcode = instr & 0x7f;
+  uint8_t funct3 = (instr >> 12) & 0x7;
+  uint8_t rd = (instr >> 7) & 0x1f;
+  uint8_t rs1 = (instr >> 15) & 0x1f;
+  bool is_jal = opcode == 0x6f;
+  bool is_jalr = opcode == 0x67 && funct3 == 0x0;
+  bool is_ret = is_jalr && rd == 0 && rs1 == 1;
+  bool is_call = (is_jal && rd == 1) || (is_jalr && rd == 1);
+  if (is_call)
+    return jump_type::call;
+  if (is_ret)
+    return jump_type::ret;
+  return jump_type::normal;
 }
-class sdb::ftrace_handler:public trace_handler{
-	private:
-		elf_handler elf;
-		jump_recognizer recog_jmp;
-		int func_depth=0;
-	public:
-		ftrace_handler(
-			string_view elf_file,
-			jump_recognizer r
-		):recog_jmp(r),func_depth(0){
-			fstream fs(string(elf_file),ios::in|ios::binary);
-			elf.load(fs);
-			_log("Loaded ELF file {}\n",elf_file);
-		}
-		virtual void handle(_ctx_ref ctx)override{
-			auto type=recog_jmp(ctx.inst);
-			if(type==jump_type::normal)return;
-			auto hint_str=type==jump_type::call?"call fun":"ret from";
-			if(type==jump_type::call)func_depth++;
 
-			auto lastf=elf.get_fun_at(ctx.lastpc);
-			auto curf=elf.get_fun_at(ctx.pc);
-			auto lastfname=lastf?lastf->name:"(unknown)";
-			auto curfname=curf?curf->name:"(unknown)";
+word_t get_target_addr_riscv(const sdb::trace_context &ctx) {
+  word_t instr = *(word_t *)ctx.inst.data();
+  uint8_t opcode = instr & 0x7f;
+  if (opcode == 0x6f) { // JAL
+    int32_t imm = ((instr >> 21) & 0x3ff) << 1 | ((instr >> 20) & 0x1) << 11 |
+                  ((instr >> 12) & 0xff) << 12 |
+                  ((instr >> 31) ? 0xfffff000 : 0);
+    return ctx.pc + imm;
+  } else if (opcode == 0x67) { // JALR
+    int32_t imm = ((instr >> 20) & 0xfff) | ((instr >> 31) ? 0xfffff000 : 0);
+    uint8_t rs1 = (instr >> 15) & 0x1f;
+    word_t base = ctx.regs[rs1];
+    return (base + imm) & ~1;
+  } else {
+    return 0;
+  }
+}
 
-			_log(
-					"0x{:08X}: "
-					"{}{} "
-					ANSI_FG_GRAY "f`{:08X}"
-					ANSI_NONE "{} last {} cur {}\n",
-				ctx.pc,
-				type==jump_type::call?ANSI_FG_YELLOW:ANSI_FG_BLUE,
-				hint_str,
-				lastf?lastf->addr:0,
-				string(func_depth,' '),
-				lastfname,curfname
-			);
+class sdb::ftrace_handler : public trace_handler {
+private:
+  elf_handler elf;
+  jump_recognizer recog_jmp;
+  int func_depth = 0;
 
-			if(type==jump_type::ret){
-				if(func_depth>0)func_depth--;
-				else _error("ret but func depth is 0");
-			}
+public:
+  ftrace_handler(string_view elf_file, jump_recognizer r)
+      : recog_jmp(r), func_depth(0) {
+    fstream fs(string(elf_file), ios::in | ios::binary);
+    elf.load(fs);
+    _log("Loaded ELF file {}\n", elf_file);
+  }
+  virtual void handle(_ctx_ref ctx) override {
+    auto type = recog_jmp(ctx.inst);
+    if (type == jump_type::normal)
+      return;
+    auto hint_str = type == jump_type::call ? "call fun" : "ret from";
+    if (type == jump_type::call)
+      func_depth++;
 
-		}
+    auto cur_f = elf.get_fun_at(ctx.pc);
+    auto cur_fname = cur_f ? cur_f->name : "(unknown)";
+
+    auto target = get_target_addr_riscv(ctx);
+
+    auto tar_f = elf.get_fun_at(target);
+    auto tar_fname = tar_f ? tar_f->name : "(unknown)";
+
+    _log("0x{:08X}: "
+         "{}{} " ANSI_FG_GRAY "f`{:08X}" ANSI_NONE "{} cur {} tar {}\n",
+         ctx.pc, type == jump_type::call ? ANSI_FG_YELLOW : ANSI_FG_BLUE,
+         hint_str, tar_f ? tar_f->addr : 0, string(func_depth, ' '), tar_fname,
+         cur_fname);
+
+    if (type == jump_type::ret) {
+      if (func_depth > 0)
+        func_depth--;
+      else
+        _error("ret but func depth is 0");
+    }
+  }
 };
 
-trace_handler_ptr sdb::make_ftrace_handler(
-	string_view elf_file,
-	jump_recognizer recog_jmp
-){
-	return std::make_shared<ftrace_handler>(elf_file,recog_jmp);
+trace_handler_ptr sdb::make_ftrace_handler(string_view elf_file,
+                                           jump_recognizer recog_jmp) {
+  return std::make_shared<ftrace_handler>(elf_file, recog_jmp);
 }
-
