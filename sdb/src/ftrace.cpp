707f74d4d8595594f4e6c27254117383604ae3f3
tracer-ysyx <tracer@ysyx.org>
2025-12-11 19:09:49 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:09:49 up 1 day, 22:45,  1 user,  load average: 0.18, 0.15, 0.17

--- sdb/src/ftrace.cpp
+++ sdb/src/ftrace.cpp
@@ -70,11 +70,14 @@ public:
     auto tar_f = elf.get_fun_at(target);
     auto tar_fname = tar_f ? tar_f->name : "(unknown)";
 
-    _log("0x{:08X}@f`{}: "
-         "{}{} " ANSI_FG_GRAY "f`{:08X}" ANSI_NONE "{} {}\n",
-         ctx.pc, cur_fname,
-         type == jump_type::call ? ANSI_FG_YELLOW : ANSI_FG_BLUE, hint_str,
-         tar_f ? tar_f->addr : 0, string(func_depth, ' '), tar_fname);
+#define ANSI_FG_FUN "\e[38;2;220;220;170m"
+
+    _log("0x{:08X}: "
+         "{}{} " ANSI_FG_GRAY "f`{:08X}" ANSI_NONE "{}" ANSI_FG_FUN
+         "{}" ANSI_NONE " from " ANSI_FG_FUN "{}\n",
+         ctx.pc, type == jump_type::call ? ANSI_FG_YELLOW : ANSI_FG_BLUE,
+         hint_str, tar_f ? tar_f->addr : 0, string(func_depth, ' '), tar_fname,
+         cur_fname);
 
     if (type == jump_type::ret) {
       if (func_depth > 0)
