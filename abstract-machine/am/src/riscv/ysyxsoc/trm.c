e2b6c2577aec3159e642772f429f3b5a5c39ccc3
tracer-ysyx <tracer@ysyx.org>
2025-12-12 01:22:59 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:22:59 up 2 days,  4:43,  1 user,  load average: 0.01, 0.06, 0.16

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -28,12 +28,10 @@ static const char mainargs[MAINARGS_MAX_LEN] =
 
 void init_serial() {
 
-	*((uint32_t*)(SERIAL_PORT)) = 0x83838383;
-
   // set UART to 8 bits, no parity, one stop bit
   // 0x3 = 0b11 : Select each character 8 bits
   // 0x80 = 0b10000000 : Divisor Latch Access bit
-  // *UART_LCR = 0x3;
+  *UART_LCR = 0x3 | 0x80;
   while (*UART_LCR == 0x0) {
     *UART_LCR = 0x80;
     *UART_DL_LSB = 'F';
