998aa4585c5690ce61e0f6cd6f8a4446f6a7b850
tracer-ysyx <tracer@ysyx.org>
2025-12-11 19:30:57 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:30:57 up 1 day, 23:06,  1 user,  load average: 0.31, 0.31, 0.27

--- sdb/src/ftrace.cpp
+++ sdb/src/ftrace.cpp
@@ -87,7 +87,7 @@ public:
 		(void)show_addr;
 
 		// " ANSI_FG_GRAY "f`{:08X}
-    _log("0x{:08X}: {} " ANSI_NONE "{}{}\n", ctx.pc,
+    _log("0x{:08X}: {}" ANSI_NONE "{}{}\n", ctx.pc,
          hint_str, string(func_depth, ' '), msg);
 
     if (type == jump_type::ret) {
