f0b3de655633fa27415fc368245b03caab224a1f
tracer-ysyx <tracer@ysyx.org>
2025-12-12 14:20:09 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:20:09 up 2 days,  9:17,  1 user,  load average: 0.07, 0.10, 0.16

--- npc/design/src/exu.scala
+++ npc/design/src/exu.scala
@@ -172,7 +172,7 @@ class EXU extends Module {
 
   memIO.arid    := 0.U
   memIO.arlen   := 0.U
-  memIO.arsize  := 2.U //memOpSize
+  memIO.arsize  := memOpSize
   memIO.arburst := 1.U
 
   when(memIO.arvalid && memIO.arready) {
