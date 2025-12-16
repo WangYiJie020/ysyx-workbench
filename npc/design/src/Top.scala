9d271c52577c99e06809d9bf324ff25e886dcea7
tracer-ysyx <tracer@ysyx.org>
2025-12-16 12:39:28 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:39:28 up 1 day, 11:21,  1 user,  load average: 3.00, 1.25, 0.59

--- npc/design/src/Top.scala
+++ npc/design/src/Top.scala
@@ -184,16 +184,16 @@ class ysyx_25100261(word_width: Int = 32) extends Module {
     ("h0f000000".U(32.W),"hffffffff".U(32.W)) -> otherReqSlave
   )))
 
-  when(io.master.bvalid && io.master.bresp === AXI4IO.BResp.DECERR){
-    printf("AXI4 DECERR on write address 0x%x\n", io.master.awaddr)
-    stop()
-    stop()
-  }
-  when(io.master.rvalid && io.master.rresp === AXI4IO.RResp.DECERR){
-    printf("AXI4 DECERR on read address 0x%x\n", io.master.araddr)
-    stop()
-    stop()
-  }
+  // when(io.master.bvalid && io.master.bresp === AXI4IO.BResp.DECERR){
+  //   printf("AXI4 DECERR on write address 0x%x\n", io.master.awaddr)
+  //   stop()
+  //   stop()
+  // }
+  // when(io.master.rvalid && io.master.rresp === AXI4IO.RResp.DECERR){
+  //   printf("AXI4 DECERR on read address 0x%x\n", io.master.araddr)
+  //   stop()
+  //   stop()
+  // }
   val SERIAL_ADDR_BASE = "h10000000".U(32.W)
   val SERIAL_ADDR_END  = "h10000020".U(32.W)
   when(io.master.awvalid && io.master.awaddr >= SERIAL_ADDR_BASE && io.master.awaddr < SERIAL_ADDR_END && io.master.awready){
