904077c242be2cb181730a0f4b6685bf56829fa2
tracer-ysyx <tracer@ysyx.org>
2026-01-19 21:41:19 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:41:19 up  9:22,  1 user,  load average: 0.56, 0.45, 0.44

---
+++ abstract-machine/am/src/riscv/ysyxsoc/soc_devreg.h
@@ -0,0 +1,46 @@
+#ifndef __DEV_REG_H__
+#define __DEV_REG_H__
+
+#include <stdint.h>
+
+typedef volatile uint8_t* dev_reg8_ptr;
+typedef volatile uint16_t* dev_reg16_ptr;
+typedef volatile uint32_t* dev_reg32_ptr;
+
+#define DEV_REG8(addr)  ((dev_reg8_ptr)(addr))
+#define DEV_REG16(addr) ((dev_reg16_ptr)(addr))
+#define DEV_REG32(addr) ((dev_reg32_ptr)(addr))
+
+#define UART_BASE 0x10000000u
+#define _UART_R(x)    (DEV_REG8(UART_BASE + (x)))
+#define UART_LCR       _UART_R(0x03)
+#define UART_DL_LSB    _UART_R(0x00)
+#define UART_DL_MSB    _UART_R(0x01)
+#define UART_FIFO_CTRL _UART_R(0x02)
+#define UART_IER       _UART_R(0x01)
+#define UART_LSR       _UART_R(0x05)
+
+
+#define SPI_BASE 0x10001000u
+#define _SPI_R(x)    (DEV_REG32(SPI_BASE + (x)))
+#define SPI_Rx0     _SPI_R(0)
+#define SPI_Rx1     _SPI_R(1)
+#define SPI_Rx2     _SPI_R(2)
+#define SPI_Rx3     _SPI_R(3)
+#define SPI_Tx0     _SPI_R(0)
+#define SPI_Tx1     _SPI_R(1)
+#define SPI_Tx2     _SPI_R(2)
+#define SPI_Tx3     _SPI_R(3)
+#define SPI_CTRL    _SPI_R(4)
+#define SPI_DIVIDER _SPI_R(5)
+#define SPI_SS      _SPI_R(6)
+
+#define _To1b(x) ((x)&0x1)
+#define MAKE_SPI_CTRL(ass,ie,lsb,tx_neg,rx_neg,go_bsy,char_len) \
+	((_To1b(ass)<<13)    | (_To1b(ie)<<12)    | (_To1b(lsb)<<11)   | \
+	 (_To1b(tx_neg)<<10) | (_To1b(rx_neg)<<9) | (_To1b(go_bsy)<<8) | \
+	 ((char_len)&0xff))
+#define IS_SPI_BUSY() _To1b((*SPI_CTRL)>>8)
+
+
+#endif // __DEV_REG_H__
--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -3,6 +3,8 @@
 #include <klib.h>
 #include <stdint.h>
 
+#include "soc_devreg.h"
+
 extern char _heap_start;
 extern char _heap_end;
 
@@ -17,15 +19,6 @@ extern char _pmem_start;
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] =
     TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
-
-#define SERIAL_PORT 0x10000000u
-
-#define UART_LCR ((volatile uint8_t *)(SERIAL_PORT + 0x03))
-#define UART_DL_LSB ((volatile uint8_t *)(SERIAL_PORT + 0x00))
-#define UART_DL_MSB ((volatile uint8_t *)(SERIAL_PORT + 0x01))
-#define UART_FIFO_CTRL ((volatile uint8_t *)(SERIAL_PORT + 0x02))
-#define UART_IER ((volatile uint8_t *)(SERIAL_PORT + 0x01))
-#define UART_LSR ((volatile uint8_t *)(SERIAL_PORT + 0x05))
 
 void init_serial() {
 
@@ -51,7 +44,7 @@ void init_serial() {
 void putch(char ch) {
   while (!(*UART_LSR & 0x20)) {
   }
-  *(uint8_t *)(SERIAL_PORT + 0x00) = ch;
+  *(uint8_t *)(UART_BASE + 0x00) = ch;
 }
 
 void halt(int code) {
