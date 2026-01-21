cb530483645bf9ded49c0f39182ae50c2c5bfadf
tracer-ysyx <tracer@ysyx.org>
2026-01-21 08:55:18 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:55:18 up  5:17,  1 user,  load average: 0.29, 0.28, 0.18

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -14,10 +14,13 @@ MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
+insert-arg: image
+	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
+
 image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
-run: image
+run: insert-arg
 	@$(MAKE) -C $(NPC_HOME) sim SIM_IMG=$(IMAGE).bin ARGS='-b'
