f1e1e519506a72f3e5ad86fc8b4a5ee228db4514
tracer-ysyx <tracer@ysyx.org>
2026-01-22 17:55:29 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:55:29 up  8:29,  1 user,  load average: 0.42, 0.21, 0.23

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -151,7 +151,8 @@ BOOT_TEXT void boot_clear(void *dst, size_t n) {
 BOOT_TEXT void _trm_init() {
   init_serial();
 
-  // putstr("xxx");
+	boot_putstr("test");
+
 
   boot_memcpy(_text_start, __text_load_start__, (size_t)__text_size__);
   boot_memcpy(_rodata_start, __rodata_load_start__, (size_t)__rodata_size__);
