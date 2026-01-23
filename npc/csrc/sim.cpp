a7f063e35bf94bf21426c5594ffbdbb40c27e308
tracer-ysyx <tracer@ysyx.org>
2026-01-23 14:59:50 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:59:50 up  9:18,  1 user,  load average: 0.28, 0.48, 0.51

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -196,7 +196,7 @@ extern "C" void sdram_read(char bank, short row, short col, short* data) {
 	assert(row>=0&&row<8192);
 	assert(col>=0&&col<512);
 	*data = sdram_data[bank][row][col];
-	printf("[DPI] [clk %ld] sdram_read bank=%02x row=%04x col=%04x data=%04x\n", sim_time, bank, row, col, *data);
+	printf("[DPI] [clk %ld] sdram_read bank=%02x row=%04x col=%04x data=%04x\n", sim_time, bank, row, col, (uint16_t)*data);
 }
 
 constexpr uint32_t SRAM_BASE = 0x0f000000u;
