17d44a6acdaa40c42280a8418817ea5393b41979
tracer-ysyx <tracer@ysyx.org>
2026-01-24 18:09:26 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:09:26 up 20:57,  1 user,  load average: 0.39, 0.39, 0.37

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -498,7 +498,7 @@ static void parse_args(int argc, char **argv) {
 void _init_dpi_logger() {
   auto formatter = std::make_unique<spdlog::pattern_formatter>();
   formatter->add_flag<sim_time_formatter>('&');
-  formatter->set_pattern("(%&) [%n] [%^%L%$] %v");
+  formatter->set_pattern("(%&) [%n] [%^%l%$] %v");
 
   auto out_file = "dpiout.log";
   auto _env_lvl_str = std::getenv("DPI_CONSOLE_LVL");
