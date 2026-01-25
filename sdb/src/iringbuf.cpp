5de604fe1cd8772aea11997f7c8f4a24e64ea139
tracer-ysyx <tracer@ysyx.org>
2026-01-25 13:01:37 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:01:37 up  6:26,  1 user,  load average: 0.33, 0.25, 0.20

--- sdb/src/iringbuf.cpp
+++ sdb/src/iringbuf.cpp
@@ -51,7 +51,8 @@ class sdb::iringbuf_trace_handler : public disasm_trace_handler {
 
 		void _dump_using_reg(_irb_inst_ctx& ctx){
 			auto dump_one_reg=[&](uint8_t r){
-				_dump(ANSI_FG_BLUE "{}" ANSI_NONE "=0x{:08x} ",ctx.reg_names[r],ctx.regs[r]);
+				if(r==0)return;
+				_dump(ANSI_FG_BLUE " {}" ANSI_NONE "=0x{:08x}",ctx.reg_names[r],ctx.regs[r]);
 			};
 			uint32_t code=*(uint32_t*)ctx.inst.data();
 			uint8_t rd=(code>>7)&0x1f;
