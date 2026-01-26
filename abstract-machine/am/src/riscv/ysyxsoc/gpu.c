421b3e68253648051089d788ad5747d12fba3a16
tracer-ysyx <tracer@ysyx.org>
2026-01-26 17:52:35 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:52:35 up 17:24,  1 user,  load average: 1.01, 1.00, 0.96

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -19,9 +19,9 @@ void __am_gpu_init() {
   gpu_h = io_read(AM_GPU_CONFIG).height;
 
 	for(int y=0;y<gpu_h;y++) {
-		printf("init line %d\n", y);
+		// printf("init line %d\n", y);
 		for(int x=0;x<gpu_w;x++) {
-			AT_FB_ADDR(x,y)[0] = 0x00ff0000;
+			AT_FB_ADDR(x,y)[0] = x<320?0x00ff0000:0x0000ff00;
 		}
 	}
 }
