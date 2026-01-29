6c55d36fb35385dfdbec4fe916c7c53c08af2740
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-01-29 11:45:45 +0800

    impl IPC impl perf handshake counter

---
+++ npc/csrc/sig_event.cpp
@@ -0,0 +1,52 @@
+#include "sig_event.hpp"
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+
+void set_logger_pattern_with_simtime(std::shared_ptr<spdlog::logger> logger);
+HandShakeDetector::HandShakeDetector() {
+  logger = spdlog::stdout_color_mt("HandShakeDetector");
+}
+void HandShakeDetector::init() {
+	set_logger_pattern_with_simtime(logger);
+	logger->set_level(spdlog::level::info);
+}
+
+auto _FullPath(const std::string &pathWithoutValidOrReady,
+               const std::string &suffix = "") {
+  return cpu_vpi_path_prefix() + pathWithoutValidOrReady + suffix;
+}
+auto _DebugPath(const std::string &pathWithoutValidOrReady,
+                const std::string &suffix = "") {
+  return "`cpu." + pathWithoutValidOrReady + suffix;
+}
+
+void HandShakeDetector::add(std::string barePath, std::string description) {
+  auto pathValid = _FullPath(barePath, "valid");
+  auto pathReady = _FullPath(barePath, "ready");
+  spdlog::debug("adding valid/ready pair: {}/{}", pathValid, pathReady);
+  vpiHandle hValid =
+      vpi_handle_by_name(const_cast<PLI_BYTE8 *>(pathValid.c_str()), nullptr);
+  if (!hValid) {
+    spdlog::error("cannot find valid signal at path {}",
+                  _DebugPath(barePath, "valid"));
+  }
+  vpiHandle hReady =
+      vpi_handle_by_name(const_cast<PLI_BYTE8 *>(pathReady.c_str()), nullptr);
+  if (!hReady) {
+    spdlog::error("cannot find ready signal at path {}",
+                  _DebugPath(barePath, "ready"));
+  }
+
+  spdlog::info("added watch for channel {} ({})", _DebugPath(barePath), description);
+  bus_list.emplace_back(hValid, hReady, description);
+}
+
+void HandShakeDetector::checkAndCountAll() {
+	for(auto &bus : bus_list){
+		if(bus.shakeHappened()){
+			bus.shake_count++;
+			logger->trace("Handshake happened on {} (total count {})",
+			              _DebugPath(bus.description), bus.shake_count);
+		}
+	}
+}
---
+++ npc/csrc/sig_event.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "vpi_user.h"
+#include <vector>
+#include <verilated.h>
+#include <verilated_vpi.h>
+
+#include "vsrc.hpp"
+
+#include "common.hpp"
+
+inline const std::string &cpu_vpi_path_prefix() {
+  static std::string prefix;
+  if (prefix.empty()) {
+    prefix = std::string("TOP.") + std::string(_STR(TOP_NAME)).substr(1);
+    prefix += ".asic.cpu.cpu.";
+  }
+  return prefix;
+}
+
+struct ValidReadyBus {
+  vpiHandle hValid;
+  vpiHandle hReady;
+
+  std::string description;
+
+	size_t shake_count = 0;
+
+  ValidReadyBus(vpiHandle hV, vpiHandle hR, std::string desc = "")
+			: hValid(hV), hReady(hR), description(desc) {}
+
+  bool shakeHappened() {
+    s_vpi_value valValid, valReady;
+    valValid.format = vpiIntVal;
+    valReady.format = vpiIntVal;
+
+    vpi_get_value(hValid, &valValid);
+    vpi_get_value(hReady, &valReady);
+
+    return (valValid.value.integer == 1) && (valReady.value.integer == 1);
+  }
+};
+
+class HandShakeDetector {
+public:
+	std::shared_ptr<spdlog::logger> logger;
+  std::vector<ValidReadyBus> bus_list;
+
+  HandShakeDetector();
+
+	void init();
+  void add(std::string pathWithoutValidOrReady, std::string description = "");
+
+	void checkAndCountAll();
+};
