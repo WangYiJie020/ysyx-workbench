ca002f5085f296d80f6d892c99bd8156a87c2aa9
tracer-ysyx <tracer@ysyx.org>
2026-01-30 14:31:00 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:31:00 up  4:38,  1 user,  load average: 1.10, 1.15, 0.98

--- npc/csrc/PerfFmt.cpp
+++ npc/csrc/PerfFmt.cpp
@@ -14,7 +14,8 @@ static void _SetTableFmt(Table &t) {
     row.format().hide_border_top();
     row.format().hide_border_bottom();
   }
-  t[1].format().show_border_top();
+  t[0].format().show_border_top();
+	t[1].format().show_border_top();
   t[t.size() - 1].format().show_border_bottom();
 }
 void _PrintTable(Table &t){
