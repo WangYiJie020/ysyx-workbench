e3ac85bb20bc6db359a3e1d63a8731d747b6a4dd
tracer-ysyx <tracer@ysyx.org>
2026-01-30 13:51:18 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:51:18 up  4:01,  1 user,  load average: 1.55, 1.20, 0.85

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -13,10 +13,26 @@
 
 int gdb_mainloop();
 
+void test_table(){
+	EXUPerfCounter exu_counter;
+	for(int i=0;i<1000;i++){
+		exu_counter.instCountOfTyp[random() % EXUPerfCounter::TYPE_NUM]++;
+		exu_counter.totalCycleOfTyp[random() % EXUPerfCounter::TYPE_NUM] += random() % 5 + 1;
+
+		exu_counter.instCountOfFmt[random() % EXUPerfCounter::FMT_NUM]++;
+		exu_counter.totalCycleOfFmt[random() % EXUPerfCounter::FMT_NUM] += random() % 5 + 1;
+	}
+	exu_counter.dumpStatistics();
+}
+
 int main(int argc, char **argv) {
 	spdlog::set_default_logger(spdlog::stdout_color_mt("sim"));
   spdlog::set_level(spdlog::level::info); // will modify all registered loggers
 	spdlog::set_pattern("[%H:%M:%S.%e][%^%-5l%$][%n] %v");
+
+	test_table();
+
+	return 0;
 
 	auto& setting = sim_get_config()->setting;
 	load_sim_setting_from_env(setting);
