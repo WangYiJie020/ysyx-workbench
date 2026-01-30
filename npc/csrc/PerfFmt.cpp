a166082948a6e70f177f002c2aa01ac75ed6e80f
tracer-ysyx <tracer@ysyx.org>
2026-01-30 14:28:07 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:28:07 up  4:36,  1 user,  load average: 0.95, 0.97, 0.88

--- npc/csrc/PerfFmt.cpp
+++ npc/csrc/PerfFmt.cpp
@@ -1,12 +1,29 @@
 #include "PerfCounter.hpp"
 #include <tabulate/table.hpp>
 
+using namespace _PerfCtrImp;
+using namespace tabulate;
+
+static void _SetTableFmt(Table &t) {
+  assert(t.size() >= 2);
+  t.format().font_align(FontAlign::right);
+  t.column(0).format().font_align(FontAlign::center);
+  t.row(0).format().font_align(FontAlign::center);
+
+  for (auto &row : t) {
+    row.format().hide_border_top();
+    row.format().hide_border_bottom();
+  }
+  t[1].format().show_border_top();
+  t[t.size() - 1].format().show_border_bottom();
+}
+
 void EXUPerfCounter::_dump(size_t *instCnts, size_t *cycCnts, size_t num,
                            const char *(*nameFunc)(int)) {
-  using namespace tabulate;
   Table t;
+  _SetTableFmt(t);
   t.add_row({"Category", "Inst Count", "Inst %", "Cycle Count", "Cycle %",
-                 "Avg CPI"});
+             "Avg CPI"});
 
   size_t totalInsts = std::accumulate(instCnts, instCnts + num, 0ull);
   size_t totalCyc = std::accumulate(cycCnts, cycCnts + num, 0ull);
@@ -26,17 +43,57 @@ void EXUPerfCounter::_dump(size_t *instCnts, size_t *cycCnts, size_t num,
         instCount == 0 ? NAN : (double)cycleCount / (double)instCount;
 
     t.add_row(RowStream{} << name << instCount << instPerc << cycleCount
-                              << cyclePerc << avgCPI);
-		t.row(i+1).format().hide_border_bottom();
-		t.row(i+1).format().hide_border_top();
+                          << cyclePerc << avgCPI);
   }
+  std::cout << t << std::endl;
+}
 
-	t[1].format().show_border_top();
-	t[num].format().show_border_bottom();
+static const char *_name_of_ifu_state(IFUStateCounter::State s) {
+  const char *names[] = {
+      "IDLE",
+      "WAIT_INST",
+      "WAIT_LATER",
+  };
+  return names[(size_t)s];
+}
+void IFUStateCounter::dumpStatistics() {
+  spdlog::info("IFU State Counter Statistics:");
+  fmt::println("total instruction fetch count: {}", totalFetchCount);
+  fmt::println("state statistics:");
+  // fmt::println("    {:10} : {:<18} {:<18}", "state", "count",
+  //              "count[exclu fetch]");
+  // auto totCycles = sim_get_cycle();
+  // for (size_t i = 0; i < STATE_NUM; i++) {
+  //   double perc = totCycles == 0
+  //                     ? NAN
+  //                     : ((double)countOfState[i] / (double)totCycles) *
+  //                     100.0;
+  //   double percNoFetch =
+  //       totCycles == 0
+  //           ? NAN
+  //           : ((double)countOfStateWhenNoFetch[i] / (double)totCycles) *
+  //           100.0;
+  //
+  //   fmt::println("    {:10} : {:>8} ({:6.3f}%) {:>8} ({:6.3f}%)",
+  //                _name_of_ifu_state((State)i), countOfState[i], perc,
+  //                countOfStateWhenNoFetch[i], percNoFetch);
+  // }
+  Table t;
+  _SetTableFmt(t);
+  t.add_row({"State", "Count", "Percent", "Count\n[exclu fetch]",
+             "Percent\n[exclu fetch]"});
 
-	t.format().font_align(FontAlign::right);
-	t.column(0).format().font_align(FontAlign::center);
-	t.row(0).format().font_align(FontAlign::center);
+  auto totCycles = sim_get_cycle();
+  for (size_t i = 0; i < STATE_NUM; i++) {
+    double perc = totCycles == 0
+                      ? NAN
+                      : ((double)countOfState[i] / (double)totCycles) * 100.0;
+    double percNoFetch =
+        totCycles == 0
+            ? NAN
+            : ((double)countOfStateWhenNoFetch[i] / (double)totCycles) * 100.0;
 
-	std::cout << t << std::endl;
+    t.add_row(RowStream{} << _name_of_ifu_state((State)i) << countOfState[i]
+                          << perc << countOfStateWhenNoFetch[i] << percNoFetch);
+  }
 }
