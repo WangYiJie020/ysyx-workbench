182345b3aa55ac18cbbda8c18651fc0b4ad87e6e
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-01-30 23:56:29 +0800

    fix switch to npc

--- npc/csrc/vsrc.hpp
+++ npc/csrc/vsrc.hpp
@@ -6,4 +6,15 @@
 #define _CONCAT(a, b) _CONCAT2(a, b)
 
 #include _STR(TOP_NAME.h)
-#include _STR(_CONCAT(TOP_NAME,__Dpi.h))
+#include _STR(_CONCAT(TOP_NAME, __Dpi.h))
+
+#include _STR(_CONCAT(TOP_NAME, __Syms.h))
+
+
+constexpr bool is_soc() { 
+#ifdef SIM_SOC
+		return true;
+#else
+		return false;
+#endif
+}
