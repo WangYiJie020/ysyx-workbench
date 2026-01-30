0a397f21dfd340ecf3b1964020ca2cbf0bf79183
tracer-ysyx <tracer@ysyx.org>
2026-01-30 18:17:01 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:17:01 up  8:13,  1 user,  load average: 2.06, 1.88, 1.94

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,89 +4,94 @@
 #include <verilated.h>
 #include <verilated_vpi.h>
 
-#include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
-#include "sim.hpp"
 #include "sdbWrap.hpp"
+#include "sim.hpp"
 
 #include "PerfCounter.hpp"
 
 int gdb_mainloop();
 
-bool is_soc(){
-	static bool isSoC = false;
-	static bool inited = false;
-	if(!inited){
-		auto arch = getenv("ARCH");
-		if(arch && std::string(arch).find("soc") != std::string::npos){
-			isSoC = true;
-		}
-		inited = true;
-	}
-	return isSoC;
+bool is_soc() {
+  static bool isSoC = false;
+  static bool inited = false;
+  if (!inited) {
+    auto arch = getenv("ARCH");
+    if (arch && std::string(arch).find("soc") != std::string::npos) {
+      isSoC = true;
+    }
+    inited = true;
+  }
+  return isSoC;
 }
 
-void test_table(){
-	EXUPerfCounter exu_counter;
-	for(int i=0;i<1000;i++){
-		exu_counter.instCountOfTyp[random() % EXUPerfCounter::TYPE_NUM]++;
-		exu_counter.totalCycleOfTyp[random() % EXUPerfCounter::TYPE_NUM] += random() % 5 + 1;
+void test_table() {
+  EXUPerfCounter exu_counter;
+  for (int i = 0; i < 1000; i++) {
+    exu_counter.instCountOfTyp[random() % EXUPerfCounter::TYPE_NUM]++;
+    exu_counter.totalCycleOfTyp[random() % EXUPerfCounter::TYPE_NUM] +=
+        random() % 5 + 1;
 
-		exu_counter.instCountOfFmt[random() % EXUPerfCounter::FMT_NUM]++;
-		exu_counter.totalCycleOfFmt[random() % EXUPerfCounter::FMT_NUM] += random() % 5 + 1;
-	}
-	exu_counter.dumpStatistics();
+    exu_counter.instCountOfFmt[random() % EXUPerfCounter::FMT_NUM]++;
+    exu_counter.totalCycleOfFmt[random() % EXUPerfCounter::FMT_NUM] +=
+        random() % 5 + 1;
+  }
+  exu_counter.dumpStatistics();
 }
 
 int main(int argc, char **argv) {
-	spdlog::set_default_logger(spdlog::stdout_color_mt("sim"));
+  spdlog::set_default_logger(spdlog::stdout_color_mt("sim"));
   spdlog::set_level(spdlog::level::info); // will modify all registered loggers
-	spdlog::set_pattern("[%H:%M:%S.%e][%^%-5l%$][%n] %v");
-	
-	if(is_soc()){
-		spdlog::info("Simulating SoC design");
-		sim_get_config()->init_pc = 0x30000000;
-	}else{
-		spdlog::info("Simulating CPU core design");
-		sim_get_config()->init_pc = 0x80000000;
-	}
-	spdlog::info("Sim init pc set to 0x{:08x}", sim_get_config()->init_pc);
+  spdlog::set_pattern("[%H:%M:%S.%e][%^%-5l%$][%n] %v");
 
-	auto& setting = sim_get_config()->setting;
-	load_sim_setting_from_env(setting);
+  if (is_soc()) {
+    spdlog::info("Simulating SoC design");
+    sim_get_config()->init_pc = 0x30000000;
+  } else {
+    spdlog::info("Simulating CPU core design");
+    sim_get_config()->init_pc = 0x80000000;
+  }
+  spdlog::info("Sim init pc set to 0x{:08x}", sim_get_config()->init_pc);
 
-  if(!sim_init(argc, argv, setting)){
-		get_dut()->final();
-		spdlog::error("sim_init failed");
-		return 1;
-	}
+  auto &setting = sim_get_config()->setting;
+  load_sim_setting_from_env(setting);
 
-	if(setting.gdb_mode){
-		gdb_mainloop();
-	}else{
-		sdb_mainloop();
-	}
+  if (!sim_init(argc, argv, setting)) {
+    get_dut()->final();
+    spdlog::error("sim_init failed");
+    return 1;
+  }
 
-	spdlog::info("sim ended");
-	dumpPerfCountersStatistics();
-	std::string perfCtrCSV = dumpPerfCounterAsCSV();
-	std::cout << "PerfCounter CSV Data:\n";
-	std::cout << perfCtrCSV << std::endl;
+  if (setting.gdb_mode) {
+    gdb_mainloop();
+  } else {
+    sdb_mainloop();
+  }
 
-	std::fstream csvFile;
-	csvFile.open("perf_counters.csv", std::ios::out);
-	if(csvFile.is_open()){
-		csvFile << perfCtrCSV;
-		csvFile.close();
-		spdlog::info("Perf counters CSV data written to 'perf_counters.csv'");
-	}else{
-		spdlog::error("Failed to write perf counters CSV data to file");
-	}
-	
-	get_dut()->final();
-	if(!setting.gdb_mode){
-		return sim_hit_good_trap() ? 0 : 1;
-	}
-	return 0;
+  spdlog::info("sim ended");
+
+  if (is_soc()) {
+    dumpPerfCountersStatistics();
+    std::string perfCtrCSV = dumpPerfCounterAsCSV();
+    std::cout << "PerfCounter CSV Data:\n";
+    std::cout << perfCtrCSV << std::endl;
+
+    std::fstream csvFile;
+    csvFile.open("perf_counters.csv", std::ios::out);
+    if (csvFile.is_open()) {
+      csvFile << perfCtrCSV;
+      csvFile.close();
+      spdlog::info("Perf counters CSV data written to 'perf_counters.csv'");
+    } else {
+      spdlog::error("Failed to write perf counters CSV data to file");
+    }
+  }
+
+  get_dut()->final();
+  if (!setting.gdb_mode) {
+    return sim_hit_good_trap() ? 0 : 1;
+  }
+  return 0;
 }
