3fb5e7d64de711b7764eaa796bc8cc6cc294cbe8
tracer-ysyx <tracer@ysyx.org>
2026-02-01 12:17:08 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:17:08 up  2:49,  1 user,  load average: 0.27, 0.30, 0.36

--- cachesim/public/src/itrace_pack.cc
+++ cachesim/public/src/itrace_pack.cc
@@ -13,8 +13,8 @@ struct pc_record {
   // 1 means [pc, pc + 1]
   // etc.
   uint8_t count;
-	// TODO:
-	// if count reaches 255, split into multiple records
+  // TODO:
+  // if count reaches 255, split into multiple records
 };
 
 struct itrace_pack_imp {
@@ -23,35 +23,46 @@ struct itrace_pack_imp {
   pc_record current;
 };
 
+#define ASSERT_READ_ONE(n)                                                     \
+  do {                                                                         \
+    auto _n = fread((n), sizeof(*(n)), 1, pack->fp);                           \
+    assert(_n == 1 && "itrace_pack: file read error");                         \
+  } while (0)
+#define ASSERT_WRITE_ONE(n)                                                    \
+  do {                                                                         \
+    auto _n = fwrite((n), sizeof(*(n)), 1, pack->fp);                          \
+    assert(_n == 1 && "itrace_pack: file write error");                        \
+  } while (0)
+
 EXTERN_C itrace_pack_t itrace_pack_create(const char *filename) {
   itrace_pack_t pack = new itrace_pack_imp;
   pack->current.pc = 0;
   pack->current.count = 0;
   pack->size = 0;
   pack->fp = fopen(filename, "wb");
-	assert(pack->fp != nullptr);
+  assert(pack->fp != nullptr);
   return pack;
 }
 static void load_one_record(itrace_pack_t pack) {
-  auto n = fread(&pack->current, sizeof(pc_record), 1, pack->fp);
-  assert(n == 1);
+	ASSERT_READ_ONE(&pack->current.pc);
+	ASSERT_READ_ONE(&pack->current.count);
 }
 static void save_one_record(itrace_pack_t pack) {
-  auto n = fwrite(&pack->current, sizeof(pc_record), 1, pack->fp);
-  assert(n == 1);
+	ASSERT_WRITE_ONE(&pack->current.pc);
+	ASSERT_WRITE_ONE(&pack->current.count);
 }
 static void init_from_file(itrace_pack_t pack) {
-	// jump to the end - size_t
-	fseek(pack->fp, -sizeof(size_t), SEEK_END);
-	// read size
+  // jump to the end - size_t
+  fseek(pack->fp, -sizeof(size_t), SEEK_END);
+  // read size
   auto n = fread(&pack->size, sizeof(size_t), 1, pack->fp);
-	assert(n == 1);
-	// jump to the beginning
-	fseek(pack->fp, 0, SEEK_SET);
-	// load the first record
-	if (pack->size > 0) {
-		load_one_record(pack);
-	}
+  assert(n == 1);
+  // jump to the beginning
+  fseek(pack->fp, 0, SEEK_SET);
+  // load the first record
+  if (pack->size > 0) {
+    load_one_record(pack);
+  }
 }
 EXTERN_C itrace_pack_t itrace_pack_open(const char *filename) {
   itrace_pack_t pack = new itrace_pack_imp;
@@ -59,23 +70,23 @@ EXTERN_C itrace_pack_t itrace_pack_open(const char *filename) {
   pack->current.count = 0;
   pack->size = 0;
   pack->fp = fopen(filename, "rb");
-	assert(pack->fp != nullptr);
+  assert(pack->fp != nullptr);
   init_from_file(pack);
   return pack;
 }
 EXTERN_C void itrace_pack_close(itrace_pack_t pack) {
-	if(pack->current.pc != 0)
-		save_one_record(pack);
-	fwrite(&pack->size, sizeof(size_t), 1, pack->fp);
+  if (pack->current.pc != 0)
+    save_one_record(pack);
+  fwrite(&pack->size, sizeof(size_t), 1, pack->fp);
   fclose(pack->fp);
   delete pack;
 }
 EXTERN_C size_t itrace_pack_size(itrace_pack_t pack) { return pack->size; }
 EXTERN_C void itrace_pack_add(itrace_pack_t pack, uint32_t pc) {
   if (pack->current.pc + pack->current.count + 1 == pc) {
-		if(pack->current.count == 0xff){
-			assert(false && "itrace_pack_add: pc_record count overflow unhandled");
-		}
+    if (pack->current.count == 0xff) {
+      assert(false && "itrace_pack_add: pc_record count overflow unhandled");
+    }
     pack->current.count += 1;
   } else {
     if (pack->current.pc != 0)
@@ -91,9 +102,10 @@ EXTERN_C uint32_t itrace_pack_pickone(itrace_pack_t pack) {
   }
   uint32_t pc = pack->current.pc + pack->current.count;
   if (pack->current.count == 0) {
-		// if(pack->current.count >= 0xff){
-		// 	fprintf(stderr, "Warning: pc_record count count huge at pc=0x%08x\n", pack->current.pc);
-		// }
+    // if(pack->current.count >= 0xff){
+    // 	fprintf(stderr, "Warning: pc_record count count huge at pc=0x%08x\n",
+    // pack->current.pc);
+    // }
     if (pack->size > 1) {
       load_one_record(pack);
     }
