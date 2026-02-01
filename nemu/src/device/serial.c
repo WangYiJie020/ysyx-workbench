ee4209da4a6374dbe8e26e5d4ebf49f3936ccf48
tracer-ysyx <tracer@ysyx.org>
2026-02-01 10:26:55 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:26:55 up 57 min,  1 user,  load average: 0.30, 0.29, 0.31

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -37,7 +37,7 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   case CH_OFFSET:
     if (is_write){
       serial_putc(serial_base[0]);
-			serial_delay = 2;
+			serial_delay = 1;
 		}
     else
       panic("do not support read");
