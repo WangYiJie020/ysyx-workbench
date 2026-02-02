a07296b15714a24cec7ef688626fe349abe2a5d6
tracer-ysyx <tracer@ysyx.org>
2026-02-02 17:08:49 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:08:49 up 21:24,  1 user,  load average: 3.89, 2.30, 1.73

--- npc/design/src/npcMem.scala
+++ npc/design/src/npcMem.scala
@@ -78,7 +78,7 @@ class AXI4MemUnit extends Module {
   val enRdDataCall = (rState === RState.waitMem) || (rState === RState.idle && sio.arvalid)
   val rdData = RawClockedNonVoidFunctionCall("pmem_read",UInt(32.W))(
     clock,
-    (!reset.asBool),
+    (!reset.asBool) && enRdDataCall,
     rdAddr
   )
   when(rState === RState.waitMem) {
