6e38ac20b307f4ae529c3e2170d2fdf5781246a3
tracer-ysyx <tracer@ysyx.org>
2026-02-02 21:06:35 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:06:35 up 1 day,  1:19,  1 user,  load average: 0.40, 0.22, 0.32

--- npc/design/src/alu.scala
+++ npc/design/src/alu.scala
@@ -38,26 +38,23 @@ class ALU extends Module {
 
   val add_sub_res = Wire(Types.UWord)
 
-  when((inbits.func7t === 0.U) || inbits.is_imm) {
-    add_sub_res := src1 + src2
-  }.otherwise {
-    add_sub_res := src1 - src2
-  }
+  // original implementation, but synthesis tool
+  // will add 140 um^2 area for unknown reason
+  //
+  // when((inbits.func7t === 0.U) || inbits.is_imm) {
+  //   add_sub_res := src1 + src2
+  // }.otherwise {
+  //   add_sub_res := src1 - src2
+  // }
   when(inbits.is_imm || (inbits.func7t === 0.U)) {
     add_sub_res := src1 + src2
   }.otherwise {
     add_sub_res := src1 - src2
   }
 
-  // when(inbits.is_imm || inbits.func7t === 0.U) {
-  //   add_sub_res := src1 + src2
-  // }.elsewhen(inbits.func7t === "b0100000".U) {
-  //   add_sub_res := src1 - src2
-  // }.otherwise {
-  //   add_sub_res := DontCare
-  // }
   val shift_res = Wire(Types.UWord)
-  when(inbits.func7t === "b0100000".U) { // sra/srai
+  // when(inbits.func7t === "b0100000".U) { // sra/srai
+  when(inbits.func7t(5)){
     shift_res := (s_src1 >> shamt).asUInt
   }.otherwise { // srl/srli
     shift_res := src1 >> shamt
