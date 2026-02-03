c0583d317e290d3b82c2039a3df84c1a12e29444
tracer-ysyx <tracer@ysyx.org>
2026-02-03 15:36:11 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:36:11 up 1 day,  9:16,  1 user,  load average: 1.89, 0.92, 0.72

--- npc/design/src/exu.scala
+++ npc/design/src/exu.scala
@@ -200,15 +200,15 @@ class EXU extends Module {
   memIO.arburst := 1.U
 
   // val memRdData = memRdRawData >> memAddrUnalignPartBitlen
-  // val memRdData = MuxLookup(memAddrUnalignPart, 0.U(8.W))(
-  //   Seq(
-  //     0.U -> memRdRawData,
-  //     1.U -> memRdRawData(31, 8).pad(32),
-  //     2.U -> memRdRawData(31, 16).pad(32),
-  //     3.U -> memRdRawData(31, 24).pad(32)
-  //   )
-  // )
-  val memRdData = memRdRawData
+  val memRdData = MuxLookup(memAddrUnalignPart, 0.U(8.W))(
+    Seq(
+      0.U -> memRdRawData,
+      1.U -> memRdRawData(31, 8).pad(32),
+      2.U -> memRdRawData(31, 16).pad(32),
+      3.U -> memRdRawData(31, 24).pad(32)
+    )
+  )
+  // val memRdData = memRdRawData
 
   val memRdDataByte = Cat(Fill(24, memRdData(7) && (~func3t(2))), memRdData(7, 0))
   val memRdDataHalf = Cat(Fill(16, memRdData(15) && (~func3t(2))), memRdData(15, 0))
