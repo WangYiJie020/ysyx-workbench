60758fc0add01d83a19a4f07dc4d6eb03f000aa7
tracer-ysyx <tracer@ysyx.org>
2026-02-03 18:50:21 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:50:21 up 1 day, 12:30,  1 user,  load average: 1.44, 2.26, 2.90

--- npc/design/src/icache.scala
+++ npc/design/src/icache.scala
@@ -15,7 +15,7 @@ class ICacheIO extends Bundle {
 
 object ICacheParameters {
   val BLOCK_SIZE         = 64
-  val BLOCK_NUM          = 16
+  val BLOCK_NUM          = 64
   val BLOCK_SIZE_INBITS  = BLOCK_SIZE * 8
   val BLOCK_SIZE_INWORDS = BLOCK_SIZE / 4
 
