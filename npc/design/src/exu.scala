0a3195eb23cb118a31adc20b20336ce0953c9338
tracer-ysyx <tracer@ysyx.org>
2026-02-03 10:10:35 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:10:35 up 1 day,  3:50,  1 user,  load average: 0.24, 0.48, 0.57

--- npc/design/src/exu.scala
+++ npc/design/src/exu.scala
@@ -275,6 +275,10 @@ class EXU extends Module {
 
   // wdata
 
+  val nxt_pc = Wire(Types.UWord)
+  val pcAddImm = dinst.pc + dinst.info.imm
+  val snpc = dinst.pc + 4.U
+
   // for now, system inst, ecall and mret has rd == 0
   // TODO: handle rd != 0 case
   io.out.bits.gpr.en := (dinst.info.rd =/= 0.U) && (dinst.info.typ =/= InstType.branch) &&
@@ -285,9 +289,9 @@ class EXU extends Module {
     Seq(
       InstType.arithmetic -> alu.io.out.bits,
       InstType.lui        -> dinst.info.imm,
-      InstType.auipc      -> (dinst.pc + dinst.info.imm),
-      InstType.jalr       -> (dinst.pc + 4.U),
-      InstType.jal        -> (dinst.pc + 4.U),
+      InstType.auipc      -> pcAddImm,
+      InstType.jalr       -> snpc,
+      InstType.jal        -> snpc,
       InstType.load       -> MuxLookup(func3t, GARBAGE_UNINIT_VALUE)(
         Seq(
           MemOp.byte     -> Cat(Fill(24, memRdData(7)), memRdData(7, 0)),
@@ -336,8 +340,7 @@ class EXU extends Module {
     }
   }
 
-  val nxt_pc = io.out.bits.nxt_pc
-  val snpc   = dinst.pc + 4.U
+  io.out.bits.nxt_pc := nxt_pc
   when(is_ecall || is_mret) {
     nxt_pc := csr_rdata
   }.otherwise {
@@ -345,13 +348,14 @@ class EXU extends Module {
       nxt_pc := (reg_v1 + dinst.info.imm) &
         Cat(Fill(Types.BitWidth.word - 1, 1.U), 0.U(1.W)) // set bit0 to 0
     }.elsewhen(dinst.info.typ === InstType.jal) {
-      nxt_pc := dinst.pc + dinst.info.imm
+      nxt_pc := pcAddImm
     }.elsewhen(dinst.info.fmt === InstFmt.branch) {
       // reuse alu
       // branch func3t
+      //
       // blt/bge 10x -> feed alu 010 -> slt
       // bltu/bgeu 11x -> feed alu 011 -> sltu
-      val isLessThan = alu.io.out.bits(0)
+      val isLessThan  = alu.io.out.bits(0)
       val take_branch = MuxLookup(func3t, false.B)(
         Seq(
           BranchOp.beq  -> (reg_v1 === reg_v2),
