98e0e3f8f7b2cec5520ceae6e1a932af547aec6c
tracer-ysyx <tracer@ysyx.org>
2026-02-05 00:21:40 +0800

    > compile NEMU ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:21:40 up  1:05,  1 user,  load average: 0.12, 0.13, 0.10

--- abstract-machine/kasan/kasan_init.c
+++ abstract-machine/kasan/kasan_init.c
@@ -3,8 +3,8 @@ void kasan_init() {
   void initialize_heap();
   void initialize_kasan();
   void call_global_ctors();
-  _no_asan_kmemset((void *)KASAN_SHADOW_MEMORY_START, KASAN_SHADOW_MEMORY_SIZE,
-                   0);
+  _no_asan_kmemset((void *)KASAN_SHADOW_MEMORY_START, 0,
+                   KASAN_SHADOW_MEMORY_SIZE);
 
   // Needed to invoke KASan globals instrumentation.
   initialize_heap();
