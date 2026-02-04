3da7d14c239d92f58bc0b38f636a5ab637008cb4
tracer-ysyx <tracer@ysyx.org>
2026-02-04 19:14:38 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:14:38 up  8:37,  1 user,  load average: 0.18, 0.45, 0.82

--- abstract-machine/klib/src/asan.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-void __asan_load4_noabort(uintptr_t addr) {
-    printf("ASan: Illegal read access at address %08x\n", (uintptr_t)addr);
-    while(1);
-}
-void __asan_handle_no_return() { }
