6f103f5a013b3041944e37eb6822b6690132f54b
tracer-ysyx <tracer@ysyx.org>
2026-02-05 15:39:00 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:39:00 up  7:30,  1 user,  load average: 1.08, 0.65, 0.47

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -136,11 +136,11 @@ extern char __data_size__[];
 
 extern char __data_extra_load_start__[];
 extern int __data_extra_size__;
-#define DATA_EXTRA_SIZE ((size_t) & __data_extra_size__)
+#define DATA_EXTRA_SIZE ((uintptr_t)(volatile void*) & __data_extra_size__)
 
 extern char __bss_extra_load_start__[];
 extern int __bss_extra_size__;
-#define BSS_EXTRA_SIZE ((size_t) & __bss_extra_size__)
+#define BSS_EXTRA_SIZE ((uintptr_t)(volatile void*) & __bss_extra_size__)
 
 extern char __sram_start__[];
 extern char __sram_end__[];
