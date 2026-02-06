3d9919cd2cba2020ca1136653c92aa141a24bc5c
tracer-ysyx <tracer@ysyx.org>
2026-02-06 18:02:30 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:02:30 up 1 day, 1 min,  1 user,  load average: 1.60, 1.70, 1.54

--- npc/design/src/Top.scala
+++ npc/design/src/Top.scala
@@ -265,10 +265,10 @@ class ysyx_25100261(word_width: Int = 32) extends Module {
   isRdAfterWr := isConflictWithEXU || isConflictWithLSU || isConflictWithWBU
   dontTouch(isRdAfterWr)
 
-  when(isRdAfterWr) {
-    isIDUWaitEXUReg := true.B
-  }.elsewhen(wbu.io.done) {
+  when(wbu.io.done){
     isIDUWaitEXUReg := false.B
+  }.elsewhen(isRdAfterWr){
+    isIDUWaitEXUReg := true.B
   }
   isIDUWaitEXU := isIDUWaitEXUReg || isRdAfterWr
   dontTouch(isIDUWaitEXU)
