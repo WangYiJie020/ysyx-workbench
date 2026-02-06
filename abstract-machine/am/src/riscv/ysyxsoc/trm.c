fef65def24fa41452f698a0940abaf3b96e122fe
tracer-ysyx <tracer@ysyx.org>
2026-02-06 13:08:26 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:08:26 up 19:18,  1 user,  load average: 0.22, 0.17, 0.13

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -126,6 +126,7 @@ extern char _data_extra_end[];
 extern char _bss_extra_start[];
 extern char _bss_extra_end[];
 #define BSS_EXTRA_SIZE ((size_t)(_bss_extra_end - _bss_extra_start))
+extern char __data_extra_size__[];
 
 extern char _ssbl_start[], _ssbl_end[];
 extern char __ssbl_load_start__[];
@@ -275,7 +276,7 @@ SSBL_TEXT void _second_boot() {
   boot_log("skip clear .bss.extra\n");
 #endif
 
-  if (DATA_EXTRA_SIZE != 0) {
+  if ((size_t)__data_extra_size__ != 0) {
     putstr(".data.extra size = ");
     putnum_base16((uint32_t)DATA_EXTRA_SIZE);
     putch('\n');
