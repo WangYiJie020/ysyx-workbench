e74aef389bcfe9cf50c720e8659aa691dde647a8
tracer-ysyx <tracer@ysyx.org>
2026-02-10 20:23:51 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:23:51 up  9:24,  1 user,  load average: 0.07, 0.17, 0.32

--- npc/design/src/lsu.scala
+++ npc/design/src/lsu.scala
@@ -54,7 +54,7 @@ class LSU extends Module {
   val isMemOp = isLoad || isStore
 
   val memIO = io.mem
-  io.out.valid := io.in.valid && ((!isMemOp) || isWaitOut || (isWaitB && memIO.bvalid))
+  io.out.valid := io.in.valid && ((!isMemOp) || isWaitOut || (isWaitB && memIO.bvalid) || (isWaitR && memIO.rvalid))
   io.in.ready := ((!isMemOp) || (isWaitOut) || (isWaitB && memIO.bvalid) || (isWaitR && memIO.rvalid)) && io.out.ready
 
   val addrAck = Mux(isLoad, io.mem.arready, io.mem.awready)
