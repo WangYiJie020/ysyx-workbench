0e91adbb4c2f70e95c1970d38a336a4d5773c098
tracer-ysyx <tracer@ysyx.org>
2026-02-10 11:03:59 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:03:59 up  2:59,  1 user,  load average: 0.06, 0.17, 0.29

--- npc/design/src/Top.scala
+++ npc/design/src/Top.scala
@@ -135,7 +135,7 @@ class ysyx_25100261(word_width: Int = 32) extends Module {
 
   val isBranchGuessWrongReg     = RegInit(false.B)
   val isIFUMeetCorrectJmpTarget = Wire(Bool())
-  isBranchGuessWrong := isBranchGuessWrongReg
+  isBranchGuessWrong := isBranchGuessWrongReg || (exu.io.out.valid && exu.io.jmpHappen)
   when(exu.io.out.valid) {
     isBranchGuessWrongReg := exu.io.jmpHappen // && exu.io.out.bits.exuWriteBack.nxt_pc =/= exu.io.out.bits.exuWriteBack.pc + 4.U
   }.elsewhen(isIFUMeetCorrectJmpTarget) {
