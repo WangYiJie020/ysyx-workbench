518427c2091e53963192508713e4d3ea368483d6
tracer-ysyx <tracer@ysyx.org>
2026-02-11 15:36:51 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:36:51 up 19:41,  1 user,  load average: 2.62, 2.23, 2.05

--- npc/csrc/PerfCounter.cpp
+++ npc/csrc/PerfCounter.cpp
@@ -257,7 +257,7 @@ void dumpPerfCounterAsCSV(std::ostream &os) {
   os << "\n" << value_row;
 }
 void dumpPerfReportOnDir(const std::string &dir) {
-  std::string prefix = "new_test_pipe";
+  std::string prefix = "pipe_with_bypass";
   std::string reportPath = dir + '/' + prefix + ".counter.rpt";
   std::ofstream reportFile(reportPath);
   if (!reportFile.is_open()) {
