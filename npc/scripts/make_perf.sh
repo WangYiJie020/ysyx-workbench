823996bacb737083b5f619a356bb4bf7faf1f364
tracer-ysyx <tracer@ysyx.org>
2026-02-12 20:31:19 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:31:19 up 1 day,  2:53,  1 user,  load average: 2.15, 2.46, 1.43

--- npc/scripts/make_perf.sh
+++ npc/scripts/make_perf.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 make clean
-export ARCH=riscv32e-ysyxsoc
-make -C ../am-kernels/benchmarks/microbench ARCH=riscv32e-ysyxsoc run VSIM_difftest=0 mainargs=train SAN_FLAGS=
+ARCH=riscv32e-ysyxsoc make -C ../am-kernels/benchmarks/microbench ARCH=riscv32e-ysyxsoc run VSIM_difftest=0 mainargs=train SAN_FLAGS=
