95f6547ef662889fd085b5cc4e2502d8d9820fe0
tracer-ysyx <tracer@ysyx.org>
2026-02-12 17:50:15 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:50:15 up 1 day, 11 min,  1 user,  load average: 0.08, 0.13, 0.30

--- npc/csrc/PerfFmt.cpp
+++ npc/csrc/PerfFmt.cpp
@@ -36,35 +36,6 @@ void HandShakeCounterManager::dumpStatistics(std::ostream &os) {
   _PrintTable(t, os);
 }
 
-// void EXUPerfCounter::_dump(size_t *instCnts, size_t *cycCnts, size_t num,
-//                            const char *(*nameFunc)(int), std::ostream &os) {
-//   Table t;
-//   t.add_row({"Category", "Inst Count", "Inst %", "Cycle Count", "Cycle %",
-//              "Avg CPI"});
-//
-//   size_t totalInsts = std::accumulate(instCnts, instCnts + num, 0ull);
-//   size_t totalCyc = std::accumulate(cycCnts, cycCnts + num, 0ull);
-//
-//   for (size_t i = 0; i < num; i++) {
-//     auto name = nameFunc((int)i);
-//     auto instCount = instCnts[i];
-//     auto cycleCount = cycCnts[i];
-//
-//     double instPerc = totalInsts == 0
-//                           ? NAN
-//                           : ((double)instCount / (double)totalInsts) * 100.0;
-//     double cyclePerc =
-//         totalCyc == 0 ? NAN : ((double)cycleCount / (double)totalCyc) *
-//         100.0;
-//
-//     double avgCPI =
-//         instCount == 0 ? NAN : (double)cycleCount / (double)instCount;
-//
-//     t.add_row(RowStream{} << name << instCount << instPerc << cycleCount
-//                           << cyclePerc << avgCPI);
-//   }
-//   _PrintTable(t, os);
-// }
 
 const char *PipeStagePerfCounter::nameOfState(int s) {
   const char *names[] = {
