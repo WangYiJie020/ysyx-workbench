608ecaa9d5628b28d37cebed3fe57b1f3bce75d9
tracer-ysyx <tracer@ysyx.org>
2026-02-12 21:26:50 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:26:50 up 1 day,  3:48,  1 user,  load average: 0.27, 0.34, 0.54

--- npc/scripts/sta_all.sh
+++ npc/scripts/sta_all.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cat $1 | grep -v '/' | sed 's/\.sv//' | while read -r name; do
-	echo "make sta $name"
-	make sta_area STA_DEST=$name > /dev/null
-done
+list=$(cat $1 | grep -v '/' | sed 's/\.sv//')
+echo $list
