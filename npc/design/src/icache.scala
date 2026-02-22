f9cbbc41ce90a946e0a6e9f5a63f759d03655921
tracer-ysyx <tracer@ysyx.org>
2026-02-22 20:58:29 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:58:29 up 2 days, 16:58,  1 user,  load average: 1.55, 1.58, 1.51

--- npc/design/src/icache.scala
+++ npc/design/src/icache.scala
@@ -199,10 +199,12 @@ class ICacheWithDirectVisit extends Module {
   io.mem.dontCareW()
   io.mem.dontCareB()
 
-  cache.io.mem.wready := DontCare
-  cache.io.mem.bvalid := DontCare
-  cache.io.mem.bresp  := DontCare
-  cache.io.mem.bid    := DontCare
+  cache.io.mem.wready  := DontCare
+  cache.io.mem.bready  := DontCare
+  cache.io.mem.bvalid  := DontCare
+  cache.io.mem.bresp   := DontCare
+  cache.io.mem.bid     := DontCare
+  cache.io.mem.awready := DontCare
 
   io.cpu.arready := Mux(isDirectVisit, io.mem.arready, cache.io.cpu.arready)
   io.mem.arvalid := Mux(isDirectVisit, io.cpu.arvalid, cache.io.mem.arvalid)
