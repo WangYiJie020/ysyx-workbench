39bb906c98869aaaacc86cbbd7a61583dbc26b62
tracer-ysyx <tracer@ysyx.org>
2026-02-24 19:44:59 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:44:59 up 3 days, 18:07,  1 user,  load average: 5.06, 4.80, 4.69

--- abstract-machine/klib/include/my_utils.h
+++ abstract-machine/klib/include/my_utils.h
@@ -9,6 +9,8 @@
 
 #define __asm_call_fence_i__() asm volatile("fence.i")
 
+#define _NoInline_ __attribute__((noinline))
+
 inline uint32_t get_mvendorid(){
 	uint32_t mvendor_id;
   asm volatile("csrr %0, mvendorid" : "=r"(mvendor_id));
