0ff149f52e11ffd0ee321fde586430f5fc68ee9f
tracer-ysyx <tracer@ysyx.org>
2026-02-25 10:57:03 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:57:03 up 4 days, 6 min,  1 user,  load average: 4.43, 4.68, 4.84

--- npc/design/src/RegFile.scala
+++ npc/design/src/RegFile.scala
@@ -94,18 +94,26 @@ class CSRIO                     extends Bundle {
   val read     = CSRegReqIO.RX.SingleRead
   val write    = CSRegReqIO.RX.Write
 }
+
 class ControlStatusRegisterFile extends Module {
   val io = IO(new CSRIO)
 
-  val mcycle64 = RegInit(0.U(64.W))
+  // val mcycle64 = RegInit(0.U(64.W))
   // mcycle64 := mcycle64 + 1.U
-  // val mcycleHi = RegInit(0.U(32.W))
-  // val mcycleLo = RegInit(0.U(32.W))
+  val mcycleHi = RegInit(0.U(32.W))
+  val mcycleLo = RegInit(0.U(32.W))
   // mcycleLo := mcycleLo + 1.U
   // when(mcycleLo === "hffffffff".U) {
   //   mcycleHi := mcycleHi + 1.U
   // }
-  // val mcycle64 = Cat(mcycleHi, mcycleLo)
+
+  val nxtMCycLoWithCarry = mcycleLo +% 1.U
+  mcycleLo := nxtMCycLoWithCarry(31, 0)
+  when(nxtMCycLoWithCarry(32)) {
+    mcycleHi := mcycleHi + 1.U
+  }
+
+  val mcycle64 = Cat(mcycleHi, mcycleLo)
 
   val mvendor_id = "h79737978".U(32.W) // ysyx
   val march_id   = "d25100261".U(32.W)
