be332aed3e5e7eaf3f1a4a6cf6d0ef0199b31f39
tracer-ysyx <tracer@ysyx.org>
2026-02-25 15:16:16 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:16:16 up 4 days,  4:25,  1 user,  load average: 1.53, 1.40, 1.51

--- npc/design/src/RegFile.scala
+++ npc/design/src/RegFile.scala
@@ -119,7 +119,12 @@ class ControlStatusRegisterFile extends Module {
 
   // Writable CSRs
   // 0: mstatus
-  val waregs = RegInit(VecInit("h00001800".U(32.W) +: Seq.fill(3)(0.U(32.W))))
+  // val waregs = RegInit(VecInit("h00001800".U(32.W) +: Seq.fill(3)(0.U(32.W))))
+
+  val mstatus = RegInit("h00001800".U(32.W))
+  val otherCSRs = Reg(Vec(3, UInt(32.W)))
+  val waregs = VecInit(mstatus +: otherCSRs.toSeq)
+
   val walut  = Seq(
     CSRAddr.mstatus -> 0.U,
     CSRAddr.mepc    -> 1.U,
