3cf039c1bf3822c3c93c1f89004b331cdf6edac4
tracer-ysyx <tracer@ysyx.org>
2026-02-26 13:18:12 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:18:12 up 4 days, 16:59,  1 user,  load average: 0.64, 0.69, 0.63

--- npc/design/src/npcMem.scala
+++ npc/design/src/npcMem.scala
@@ -123,9 +123,8 @@ class AXI4MemUnit extends Module {
   val enRdDataCall = WireDefault((rState === RState.waitMem) || (rState === RState.idle && sio.arvalid))
   dontTouch(enRdDataCall)
 
-  val mem = Module(new MaskedRdWrMem(1024*1024*128, None)) 
-  mem.io:=DontCare
-  dontTouch(mem.io)
+  val mem = Module(new MaskedRdWrMem(1024 * 1024 * 128, None))
+  mem.io := DontCare
 
   when(rState === RState.waitMem) {
     // rdFIFO.io.enq.bits  := UnclockedCallNonVoidDPIC("pmem_read", UInt(32.W))(
