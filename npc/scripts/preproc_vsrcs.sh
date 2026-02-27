e09ccabae7e911a9033e842d7e37809325905479
tracer-ysyx <tracer@ysyx.org>
2026-02-27 14:27:29 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:27:29 up  8:46,  1 user,  load average: 0.34, 0.47, 0.37

--- npc/scripts/preproc_vsrcs.sh
+++ npc/scripts/preproc_vsrcs.sh
@@ -9,8 +9,15 @@ SEARCH_DIR=$1
 
 VSRCS=$(find $SEARCH_DIR -name "*.sv")
 
+echo "# Inserting verilator public_on/off in $SEARCH_DIR"
+
 for src in $VSRCS; do
 	sed -i '1i /*verilator public_on*/' $src
 	sed -i -e '$a /*verilator public_off*/' $src
+done
+
+echo "# Renaming layer$ to $2_ in $SEARCH_DIR"
+
+for src in $VSRCS; do
 	sed -i "s/layer\$\(\w\+\)/$2_\1/g" $src
 done
