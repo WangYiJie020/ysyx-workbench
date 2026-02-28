018ce83aba4fd4d681e66961949c28f9190cf5f1
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-02-28 13:20:36 +0800

    ls /usr after install LLVM

--- npc/scripts/deplibs.mk
+++ npc/scripts/deplibs.mk
@@ -14,8 +14,9 @@ ifeq ($(CLANG_VERSION_OLDER_THAN_15), 1)
 	wget 'https://apt.llvm.org/llvm.sh' && chmod +x llvm.sh
 	@echo "Installing LLVM 21"
 	sudo ./llvm.sh 21
-	$(shell sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-21 100)
-	$(shell sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-21 100)
+	ls /usr/bin | grep clang
+	ls /usr/lib | llvm
+	ls /usr/lib/clang
 endif
 	touch $@
 
