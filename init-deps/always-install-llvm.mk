c53b36638c3c2970c7a6023b5dd7eee170b9f4ff
tracer-ysyx <tracer@ysyx.org>
2026-03-04 16:07:20 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:07:20 up 3 days,  4:11,  1 user,  load average: 1.04, 1.06, 1.08

--- init-deps/always-install-llvm.mk
+++ init-deps/always-install-llvm.mk
@@ -1,7 +1,7 @@
-always-make.mk:
+update-buildtools:
 	$(YSYX_HOME)/init-deps/check-and-install-buildtools.sh
 
 # not install when clean anything
 # ifneq ($(filter clean%,$(MAKECMDGOALS)),)
--include always-make.mk
+# -include always-make.mk
 # endif
--- npc/scripts/deplibs.mk
+++ npc/scripts/deplibs.mk
@@ -1,4 +1,4 @@
-include $(YSYX_HOME)/init-deps/always-install-llvm.mk
+# include $(YSYX_HOME)/init-deps/always-install-llvm.mk
 
 # sdb
 INC_PATH += $(abspath ../sdb/include)
@@ -7,6 +7,7 @@ ARCHIVES += $(NVBOARD_ARCHIVE) $(abspath ../sdb/build/libsdb.a)
 
 SDB_BUILD_LIB = $(abspath ../sdb/build/libsdb.a)
 $(SDB_BUILD_LIB):
+	$(YSYX_HOME)/init-deps/check-and-install-buildtools.sh
 	@mkdir -p $(dir $@)
 	@flock $@.lock -c '\
 		if [ ! -f $(SDB_BUILD_LIB) ]; then \
@@ -43,7 +44,7 @@ $(SIM_DEP_LIBS_CLONE_DONE):
 	@touch $@
 
 SIM_DEP_LIBS_BUILD_DONE = $(DEPS_DIR)/build.done
-$(SIM_DEP_LIBS_BUILD_DONE): $(SIM_DEP_LIBS_CLONE_DONE) $(SDB_BUILD_LIB)
+$(SIM_DEP_LIBS_BUILD_DONE): $(SDB_BUILD_LIB) $(SIM_DEP_LIBS_CLONE_DONE) 
 	@+./scripts/dev-init/build_deps.sh $(DEPS_DIR)
 	@touch $@
 
