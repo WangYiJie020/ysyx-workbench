9af07ed214ce770259ba25a2897cbc69edf66430
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-03-04 15:19:44 +0800

    discard check clang version in am

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -5,17 +5,17 @@ ASFLAGS       += $(COMMON_CFLAGS) -O0 -g
 LDFLAGS       += #-melf64lriscv
 
 
-CLANG_VERSION_MAJOR := $(shell $(CC) -dumpversion | cut -f1 -d.)
-CLANG_VERSION_OLDER_THAN_15 := $(shell [ $(CLANG_VERSION_MAJOR) -lt 15 ] && echo 1 || echo 0)
-
-$(info Clang version $(CLANG_VERSION_MAJOR))
-ifeq ($(CLANG_VERSION_OLDER_THAN_15),1)
-$(warning # WARN $(CC) version should be at least 15)
-# $(info fuck CI)
-# RISCV_MARCH_EXT_CSRS_AND_FENCE_I := 
-# COMMON_CFLAGS += -Wno-error=unused-command-line-argument
-else
-endif
+# CLANG_VERSION_MAJOR := $(shell $(CC) -dumpversion | cut -f1 -d.)
+# CLANG_VERSION_OLDER_THAN_15 := $(shell [ $(CLANG_VERSION_MAJOR) -lt 15 ] && echo 1 || echo 0)
+#
+# $(info Clang version $(CLANG_VERSION_MAJOR))
+# ifeq ($(CLANG_VERSION_OLDER_THAN_15),1)
+# $(warning # WARN $(CC) version should be at least 15)
+# # $(info fuck CI)
+# # RISCV_MARCH_EXT_CSRS_AND_FENCE_I := 
+# # COMMON_CFLAGS += -Wno-error=unused-command-line-argument
+# else
+# endif
 RISCV_MARCH_EXT_CSRS_AND_FENCE_I := _zicsr_zifencei
 
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
