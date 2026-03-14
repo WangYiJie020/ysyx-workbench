e0d34b0b475a3d8ac97872d363089e8042729f12
tracer-ysyx <tracer@ysyx.org>
2026-03-14 22:10:38 +0800

    > sim RTL ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:10:38 up 22:43,  1 user,  load average: 0.27, 1.36, 1.70

--- sdb/src/selfloop.cpp
+++ sdb/src/selfloop.cpp
@@ -1,13 +1,14 @@
 #include <cstdint>
-#include <tracers.hpp>
 #include <sdb.hpp>
+#include <tracers.hpp>
 
-void sdb::self_loop_trace_handler::handle(_ctx_ref ctx){
-	uint32_t inst=*(uint32_t*)ctx.inst.data();
-	if(inst==0x0000006f){ // riscv jal x0,0
-		// self loop detected, stop the cpu
-		_log("deadloop (jump to self) detected at pc={:#x}, stop the cpu\n",ctx.pc);
-		name = "self-loop-detector";
-	}
-	_req_stop();
+void sdb::self_loop_trace_handler::handle(_ctx_ref ctx) {
+  uint32_t inst = *(uint32_t *)ctx.inst.data();
+  if (inst == 0x0000006f) { // riscv jal x0,0
+    // self loop detected, stop the cpu
+    _log("deadloop (jump to self) detected at pc={:#x}, stop the cpu\n",
+         ctx.pc);
+    name = "self-loop-detector";
+    _req_stop();
+  }
 }
