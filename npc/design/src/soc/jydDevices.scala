a2f8da6c82a05ea448b828609b64347a521979b2
tracer-ysyx <tracer@ysyx.org>
2026-03-14 21:27:33 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:27:33 up 22:00,  1 user,  load average: 0.62, 1.29, 1.43

--- npc/design/src/soc/jydDevices.scala
+++ npc/design/src/soc/jydDevices.scala
@@ -93,7 +93,7 @@ class Timer extends Module {
   val timer = RegInit(0.U(32.W))
   timer := Mux(state === State.ticking, timer + 1.U, timer)
 
-  when(io.wvalid) {
+  when(io.wvalid && io.wready) {
     when(io.wdata === MAGIC_START) {
       printf("Timer start!\n")
     }.elsewhen(io.wdata === MAGIC_STOP) {
