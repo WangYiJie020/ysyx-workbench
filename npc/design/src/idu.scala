13005d0ab5ca2a5ddcde6766a7efd729cd3cb4f9
tracer-ysyx <tracer@ysyx.org>
2026-03-19 22:00:10 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:00:10 up 1 day, 19:03,  1 user,  load average: 1.16, 1.25, 0.77

--- npc/design/src/idu.scala
+++ npc/design/src/idu.scala
@@ -288,6 +288,6 @@ class IDU(
   io.in.ready  := (io.out.ready && !needStall) || io.flush
   io.out.valid := io.in.valid && !needStall && !io.flush
 
-  // val dbgOutInst = WireDefault(io.in.bits.code.get)
-  // dontTouch(dbgOutInst)
+  val dbgOutInst = WireDefault(io.in.bits.code.get)
+  dontTouch(dbgOutInst)
 }
