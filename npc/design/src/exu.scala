47b2e8b3a6f4aa9d0089c5ff17521eff5c947e34
tracer-ysyx <tracer@ysyx.org>
2026-03-19 18:50:08 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:50:08 up 1 day, 16:19,  1 user,  load average: 0.76, 0.37, 0.38

--- npc/design/src/exu.scala
+++ npc/design/src/exu.scala
@@ -29,8 +29,6 @@ class EXUStageCalcOut(
 
   val pcAddImm = Types.UWord
 
-  val imm = Types.UWord
-
   val takeBranch = Bool()
 
   val gprWeEn = Bool()
@@ -71,7 +69,6 @@ class EXUStageCalc(
   io.out.valid := io.in.valid && !io.flush
 
   val instIMM = dinst.imm
-  io.out.bits.imm      := instIMM
 
   io.out.bits.pcAddImm := dinst.pc.get + instIMM
   io.out.bits.dinst    := dinst
@@ -266,7 +263,7 @@ class EXUStageChooseNxt(
   writeBackInfo.gpr.data := Mux1H(
     Seq(
       isTypArithmetic         -> io.in.bits.aluOut,
-      isTypLUI                -> io.in.bits.imm,
+      isTypLUI                -> io.in.bits.dinst.imm,
       isTypAUIPC              -> pcAddImm,
       (isTypJALR || isTypJAL) -> snpc,
       io.in.bits.isTypSys     -> sysInstWrBackData
