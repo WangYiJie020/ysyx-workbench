3343b20b0a9b9cdc1cbab11393b39d206d1496f7
tracer-ysyx <tracer@ysyx.org>
2026-03-19 20:42:48 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:42:48 up 1 day, 17:48,  1 user,  load average: 0.50, 0.52, 0.42

--- npc/design/src/ifu.scala
+++ npc/design/src/ifu.scala
@@ -29,7 +29,7 @@ class IFU extends Module {
   io.mem.dontCareNonLiteAR()
 
   val pcReg     = RegEnable(io.pc.bits(31,2), io.pc.fire)
-  val pc        = Mux(io.pc.fire, io.pc.bits, pcReg ## 0.U(2.W))
+  val pc        = Mux(io.pc.fire, io.pc.bits(31,2), pcReg) ## 0.U(2.W)
   val predNxtPC = RegEnableReadNew(io.predictedNextPC, io.pc.fire)
   // dontTouch(pc)
   val state     = RegInit(State.idle)
