458de5d7fd6b16a55dad2181c492e411e856518c
tracer-ysyx <tracer@ysyx.org>
2026-03-19 10:56:10 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:56:10 up 1 day, 10:36,  1 user,  load average: 0.33, 0.55, 0.54

--- npc/design/src/Config.scala
+++ npc/design/src/Config.scala
@@ -4,6 +4,6 @@ import chisel3._
 
 object Config {
   // not affect area, yosys can optimize it away
-  val genStageLog: Boolean = true
+  val genStageLog: Boolean = false
   val useBTBAndBP: Boolean = false
 }
