ff08918513f60bd0b0d9b96abde2b71f27cf8713
tracer-ysyx <tracer@ysyx.org>
2026-03-19 11:21:01 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:21:01 up 1 day, 11:01,  1 user,  load average: 0.20, 0.47, 0.52

--- npc/design/src/ifu.scala
+++ npc/design/src/ifu.scala
@@ -28,8 +28,10 @@ class IFU extends Module {
   io.mem.dontCareB()
   io.mem.dontCareNonLiteAR()
 
-  val pcReg     = RegEnable(io.pc.bits(31,2), io.pc.fire)
-  val pc        = Mux(io.pc.fire, io.pc.bits, pcReg ## 0.U(2.W))
+  // I dont know why use 32b PC will have smaller area than 30b PC for
+  // full module area
+  val pcReg     = RegEnable(io.pc.bits, io.pc.fire)
+  val pc        = Mux(io.pc.fire, io.pc.bits, pcReg)
   val predNxtPC = RegEnableReadNew(io.predictedNextPC, io.pc.fire)
   // dontTouch(pc)
   val state     = RegInit(State.idle)
