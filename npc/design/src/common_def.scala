57745f5d43f6de3fb82b2ca4b2b60e51869d47ee
tracer-ysyx <tracer@ysyx.org>
2026-03-23 23:29:58 +0800

    > generate verilog ysyx_25100261 Wang Yi Jie Linux LAPTOP-4JBL9JGU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:29:58 up 10:23,  1 user,  load average: 0.87, 0.45, 0.31

--- npc/design/src/common_def.scala
+++ npc/design/src/common_def.scala
@@ -111,7 +111,11 @@ class InstCodeNoCExt extends Bundle {
   // no support for compressed instruction,
   // the 2 least significant bits of instruction code are always 11b
   val raw = UInt(30.W)
-  def get = Cat(raw, "b11".U(2.W))
+  def get = {
+    val dbgRaw = Cat(raw, "b11".U(2.W))
+    dontTouch(dbgRaw)
+    dbgRaw
+  }
   def eq(that: UInt) = this.get === that
 
   def func3t = this.get(14, 12)
